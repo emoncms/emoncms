15f10cdce8724cb3a1ba8e3fde8d276f24f70d38
Ildefonso Martinez <i.martinezmarchena@gmail.com>
2012-10-26 13:33:53 +0200

    Updated Twitter Bootstrap 2.1.0 ( http://blog.getbootstrap.com/2012/08/20/bootstrap-2-1-0-released/ )

--- Theme/basic/theme.php
+++ Theme/basic/theme.php
@@ -36,7 +36,7 @@
      *------------------------------------------------------
      */
     ?>
-    <div class="navbar navbar-fixed-top">
+    <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
           <?php if (!isset($runmenu)) $runmenu = '';
