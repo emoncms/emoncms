fc3e15b308e049c582548ccd4bd14e60d89fdf3d
Trystan <trystan@trystan-laptop.(none)>
2012-12-03 21:22:40 +0000

    force reload

--- Modules/dashboard/Views/dashboard_list_view.php
+++ Modules/dashboard/Views/dashboard_list_view.php
@@ -23,7 +23,7 @@ global $session, $path, $useckeditor;
 
 <!-- tool menu TODO:is the same at dashboard_thumb_view so it could be include from one place to share code -->
 <div style="float:right">
-  <a href="#" onclick="$.ajax({type: 'POST',url:'<?php echo $path; ?>dashboard/new.json',success: location.reload()});"><i class="icon-plus-sign"></i></a>
+  <a href="#" onclick="$.ajax({type: 'POST',url:'<?php echo $path; ?>dashboard/new.json',success: location.reload(true)});"><i class="icon-plus-sign"></i></a>
   <a href="<?php echo $path; ?>dashboard/thumb" title="<?php echo _("Thumb view"); ?>"><i class="icon-th-large"></i></a>
   <!--<a href="<?php echo $path; ?>dashboard/list"><i class="icon-th-list"></i></a>-->
 </div>
@@ -72,23 +72,23 @@ global $session, $path, $useckeditor;
           if ($dashboard['main']) { ?>             
               <i title="<?php echo _('This is the main dashboard'); ?>" class='icon-star'></i>
           <?php } else { ?>          
-          <a href="#" title="<?php echo _('Set main dashboard'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'main=1&id=<?php echo $dashboard['id'] ?>',success : location.reload()});"><i class='icon-star-empty'></i></a>
+          <a href="#" title="<?php echo _('Set main dashboard'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'main=1&id=<?php echo $dashboard['id'] ?>',success : location.reload(true)});"><i class='icon-star-empty'></i></a>
         <?php } ?> 
       </td>
       <td>
         <?php
           if ($dashboard['published']) { ?>           
-            <a href="#" title="<?php echo _('Unpublish dashboard'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'published=0&id=<?php echo $dashboard['id'] ?>',success : location.reload()});"><i class='icon-ok'></i></a>
+            <a href="#" title="<?php echo _('Unpublish dashboard'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'published=0&id=<?php echo $dashboard['id'] ?>',success : location.reload(true)});"><i class='icon-ok'></i></a>
           <?php } else { ?>
-            <a href="#" title="<?php echo _('Publish dashboard'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'published=1&id=<?php echo $dashboard['id'] ?>',success : location.reload()});"><i class='icon-remove'></i></a>
+            <a href="#" title="<?php echo _('Publish dashboard'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'published=1&id=<?php echo $dashboard['id'] ?>',success : location.reload(true)});"><i class='icon-remove'></i></a>
           <?php } ?>
       </td>
       <td>
         <?php 
           if ($dashboard['public']) { ?>           
-            <a href="#" title="<?php echo _('Make dashboard private'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'public=0&id=<?php echo $dashboard['id'] ?>',success : location.reload()});"><i class='icon-globe'></i></a>
+            <a href="#" title="<?php echo _('Make dashboard private'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'public=0&id=<?php echo $dashboard['id'] ?>',success : location.reload(true)});"><i class='icon-globe'></i></a>
           <?php } else { ?>
-            <a href="#" title="<?php echo _('Make dashboard public'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'public=1&id=<?php echo $dashboard['id'] ?>',success : location.reload()});"><i class='icon-lock'></i></a>
+            <a href="#" title="<?php echo _('Make dashboard public'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/setconf  ',data : 'public=1&id=<?php echo $dashboard['id'] ?>',success : location.reload(true)});"><i class='icon-lock'></i></a>
           <?php } ?>
       </td>  
       <td>
@@ -96,8 +96,8 @@ global $session, $path, $useckeditor;
           <a href="#" title="<?php echo _('Draw'); ?>" onclick="$(window.location).attr('href',path+'dashboard/edit&id=<?php echo $dashboard['id']; ?>')"><i class='icon-edit'></i></a>
 	  			<?php if ($useckeditor) { ?><a href="#" onclick="$(window.location).attr('href',path+'dashboard/ckeditor&id=<?php echo $dashboard['id']; ?>')"><img src="<?php echo $path; ?>/Includes/editors/images/ckicon.png" style="margin-top:-5px;" /></a><?php } ?>
           <a href="#" title="<?php echo _('View'); ?>" onclick="$(window.location).attr('href',path+'dashboard/view&id=<?php echo $dashboard['id']; ?>')"><i class='icon-eye-open'></i></a>
-          <a href="#" title="<?php echo _('Delete'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/delete',data : '&id=<?php echo $dashboard['id']; ?>',success : location.reload()});"><i class='icon-trash'></i></a>
-          <a href="#" title="<?php echo _('Clone'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/clone',data : '&id=<?php echo $dashboard['id']; ?>',success : location.reload()});"><i class='icon-random'></i></a>          
+          <a href="#" title="<?php echo _('Delete'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/delete',data : '&id=<?php echo $dashboard['id']; ?>',success : location.reload(true)});"><i class='icon-trash'></i></a>
+          <a href="#" title="<?php echo _('Clone'); ?>" onclick="$.ajax({type : 'POST',url :  path + 'dashboard/clone',data : '&id=<?php echo $dashboard['id']; ?>',success : location.reload(true)});"><i class='icon-random'></i></a>          
         </div>            
       </td> 
       <td>
@@ -119,7 +119,7 @@ global $session, $path, $useckeditor;
 <div class="alert alert-block">
   <h4 class="alert-heading">No dashboards created</h4>
   <p>Maybe you would like to add your first dashboard using the 
-  <a href="#" onclick="$.ajax({type: 'POST',url:'<?php echo $path; ?>dashboard/new.json',success: location.reload()});"><i class="icon-plus-sign"></i></a> button.
+  <a href="#" onclick="$.ajax({type: 'POST',url:'<?php echo $path; ?>dashboard/new.json',success: location.reload(true)});"><i class="icon-plus-sign"></i></a> button.
 </div>
 <?php  } ?>
 
