b76a1418c1af8ad3a28e06b2a4c616ad71762d8d
Trystan <trystan@trystan-laptop.(none)>
2012-12-04 19:29:56 +0000

    Changes to make stack widget work with new energy module

--- Modules/dashboard/Views/js/widgets/stack/stack_render.js
+++ Modules/dashboard/Views/js/widgets/stack/stack_render.js
@@ -1,12 +1,10 @@
-
 function stack_widgetlist()
 {
   var widgets = {
     "stack": 
     {
-      "offsetx":-150,"offsety":-200,"width":300,"height":400,
-      "menu":"Widgets",
-      "options":["test"]
+      "offsetx":-50,"offsety":-200,"width":100,"height":440,
+      "menu":"Widgets"
     }
   }
   return widgets;
@@ -21,16 +19,8 @@ function stack_init()
 function stack_draw()
 {
   $('.stack').each(function(index) {
-
-    var stacks = [];
-
-    stacks[0] = {'name':"Average", 'stack':[], 'height':99};
-    stacks[0]['stack'][0] = {'kwhd':9, 'color':0, 'name':"Electric" };
-    stacks[0]['stack'][1] = {'kwhd':45, 'color':0, 'name':"Heating" }; 
-    stacks[0]['stack'][2] = {'kwhd':46, 'color':0, 'name':"Transport" };
-
     var id = "can-"+$(this).attr("id");
-    draw_stacks(stacks,id,300,400);
+    draw_stacks(stacks,id,300,460);
   });
 }
 
