f430d9eb68c4ccb22b0a249f49f50c0bf98727eb
mezz64 <mezz@johnmihalic.com>
2013-01-02 12:36:18 -0500

    Update Modules/dashboard/Views/js/widgets/led/led_render.js
    Switch indicator colors around

--- Modules/dashboard/Views/js/widgets/led/led_render.js
+++ Modules/dashboard/Views/js/widgets/led/led_render.js
@@ -1,4 +1,3 @@
-
 function led_widgetlist()
 {
   var widgets = {
@@ -58,18 +57,18 @@ function draw_led(circle,status)
 
   var radgrad = circle.createRadialGradient(40,40,0,40,40,20);  
 
-if (status==0) {                   // red
+if (status==0) {                   // GRAY
+  radgrad.addColorStop(0, '#736F6E');  
+  radgrad.addColorStop(0.9, '#4A4344');
+} else if (status>0 && status <=1) {            // RED - HEAT ON
   radgrad.addColorStop(0, '#F75D59');  
   radgrad.addColorStop(0.9, '#C11B17');  
-} else if (status>0 && status <=1) {            // green
-  radgrad.addColorStop(0, '#A7D30C');  
-  radgrad.addColorStop(0.9, '#019F62');  
-} else if (status>1 && status <=2) {           // grey
-  radgrad.addColorStop(0, '#736F6E');  
-  radgrad.addColorStop(0.9, '#4A4344');  
-} else if (status>2 && status <=3) { 		  //Blue
+} else if (status>1 && status <=2) {           // BLUE - COOL ON
   radgrad.addColorStop(0, '#00C9FF');  
   radgrad.addColorStop(0.9, '#00B5E2');  
+} else if (status>2 && status <=3) { 		  //GREEN
+  radgrad.addColorStop(0, '#A7D30C');  
+  radgrad.addColorStop(0.9, '#019F62');
 } else if (status>3 && status <=4) {		  // Purple
   radgrad.addColorStop(0, '#FF5F98');  
   radgrad.addColorStop(0.9, '#FF0188');  
@@ -80,6 +79,7 @@ if (status==0) {                   // red
   radgrad.addColorStop(0, '#000000');  
   radgrad.addColorStop(0.9, '#000000');  
 }
+
 
   radgrad.addColorStop(1, 'rgba(1,159,98,0)');
   // draw shapes  
