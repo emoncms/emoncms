f5a1bb41a55befa2da481f39f8f1ccaeecea9036
mezz64 <mezz@johnmihalic.com>
2013-01-11 16:45:12 -0500

    Update Modules/vis/visualisations/multigraph.js
    maybe add tooltips?

--- Modules/vis/visualisations/multigraph.js
+++ Modules/vis/visualisations/multigraph.js
@@ -90,6 +90,7 @@ function multigraph_init(element)
         "<input id='zoomout' type='button' value='-'/>"+
         "<input id='left' type='button' value='<'/>"+
         "<input id='right' type='button' value='>'/>"+
+        "<input id='enableTooltip' type='checkbox' checked >"+
 
       "</div>"+
     "</div>"
@@ -125,3 +126,57 @@ function multigraph_init(element)
   $('.time').click(function () {inst_timewindow($(this).attr("time")); vis_feed_data();});
   //-----------------------------------------------------------------------------------------------
 }
+
+
+function showTooltip(x, y, contents) {
+        $('<div id="tooltip">' + contents + '</div>').css( {
+            position: 'absolute',
+            display: 'none',
+            top: y + 5,
+            left: x + 5,
+            border: '1px solid #fdd',
+            padding: '2px',
+            'background-color': '#fee',
+            opacity: 0.80
+        }).appendTo("body").fadeIn(200);
+    }
+ 
+    var previousPoint = null;
+    //$("#placeholder").bind("plothover", function (event, pos, item) {
+    $("#graph").bind("plothover", function (event, pos, item) {
+        $("#x").text(pos.x.toFixed(2));
+        $("#y").text(pos.y.toFixed(2));
+ 
+        if ($("#enableTooltip:checked").length > 0) {
+            if (item) {
+                if (previousPoint != item.dataIndex) {
+                    previousPoint = item.dataIndex;
+                    
+                    $("#tooltip").remove();
+                    var x = item.datapoint[0].toFixed(2),
+                        y = item.datapoint[1].toFixed(2);
+  
+  // create a new javascript Date object based on the timestamp
+var date = new Date(parseInt(x));
+// hours part from the timestamp
+var hours = ("0" + date.getHours()).slice (-2);
+// minutes part from the timestamp
+var minutes = ("0" + date.getMinutes()).slice (-2);
+// seconds part from the timestamp
+var seconds = ("0" + date.getSeconds()).slice (-2);
+  
+ 
+// will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes + ':' + seconds;
+ 
+ 
+                    showTooltip(item.pageX, item.pageY,
+                                item.series.label + " at " + formattedTime   + " = " + y);
+                }
+            }
+            else {
+                $("#tooltip").remove();
+                previousPoint = null;            
+            }
+        }
+    });
