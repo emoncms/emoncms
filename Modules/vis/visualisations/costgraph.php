0f5eed742d203d014efd10bd60e0e5d681193301
mezz64 <mezz@johnmihalic.com>
2013-01-18 16:25:35 -0500

    Update Modules/vis/visualisations/costgraph.php

--- Modules/vis/visualisations/costgraph.php
+++ Modules/vis/visualisations/costgraph.php
@@ -86,6 +86,9 @@
   {
     if (valid) graph_data = get_feed_data(feedid,start,end,500);
     <!--graph_data = (graph_data * cost)+(rider/31);-->
+    for(var i=0;i<graph_data.length;i++){
+      document.write("<b>arr["+i+"] is </b>=>"+graph_data[i]+"<br>");
+      }
     plot();
   }
 
