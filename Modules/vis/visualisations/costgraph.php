422e3d4141554a80ed09be65822a3ad546d5e4aa
mezz64 <mezz@johnmihalic.com>
2013-01-21 15:24:55 -0500

    Update Modules/vis/visualisations/costgraph.php

--- Modules/vis/visualisations/costgraph.php
+++ Modules/vis/visualisations/costgraph.php
@@ -73,7 +73,7 @@
   
   $('#graph').width($('#graph_bound').width());
   $('#graph').height($('#graph_bound').height()-25);
-  if (embed) $('#graph').height($(window).height()-50);
+  if (embed) $('#graph').height($(window).height()-100);
 
   var timeWindow = (3600000*24.0*30);  			//Initial time window
   var start = ((new Date()).getTime())-timeWindow;		//Get start time
@@ -87,7 +87,7 @@
 
   $(window).resize(function(){
     $('#graph').width($('#graph_bound').width());
-    if (embed) $('#graph').height($(window).height()-50);
+    if (embed) $('#graph').height($(window).height()-100);
     plot();
   });
 
