a0aa308deab51208f0e83cf7f4572cb9823b0da7
trystan <trystan@emoncms.org>
2013-03-21 20:25:59 +0000

    minor name change

--- Theme/menu_view.php
+++ Theme/menu_view.php
@@ -18,7 +18,7 @@
 
   if ($session['write']) $menu_right[] = array('name'=>"<b>Docs</b>", 'path'=>"site/docs", 'order' => 0 );
   if (!$session['write']) $menu_right[] = array('name'=>"Log In", 'path'=>"user/login", 'order' => -1 );
-  if ($session['write']) $menu_right[] = array('name'=>"<b>Status</b>", 'path'=>"site/docs/status", 'order' => 0 );
+  if ($session['write']) $menu_right[] = array('name'=>"<b>Troubleshooting</b>", 'path'=>"site/docs/status", 'order' => 0 );
 
 
 ?>
