dd20fe59c9fd6a1452238b332c284281715c9dd6
Trystan <trystan@trystan-laptop.(none)>
2013-04-14 19:57:01 +0100

    local time

--- Modules/feed/feed.js
+++ Modules/feed/feed.js
@@ -20,6 +20,21 @@ var feed = {
     $.ajax({ url: path+"feed/delete.json", data: "id="+id, async: false, success: function(data){} });
   },
 
+
+  // if ($route->action == 'data') $result = $feed->get_data(get('id'),get('start'),get('end'),get('dp'));
+  'get_data':function(feedid,start,end,dp)
+  {
+    var feedIn = [];
+    $.ajax({                                      
+      url: path+'feed/data.json',                         
+      data: "&apikey="+apikey+"&id="+feedid+"&start="+start+"&end="+end+"&dp="+dp,
+      dataType: 'json',
+      async: false,                      
+      success: function(data_in) { feedIn = data_in; } 
+    });
+    return feedIn;
+  },
+
   'get_kwhatpowers':function(feedid,points)
   {
     var feedIn = [];
