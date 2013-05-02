dfcaa97cb1eacc4259f016db83af0fd25e0a5dc1
Ildefonso Martinez <i.martinezmarchena@gmail.com>
2013-05-02 18:10:18 +0200

    Clean code for next step...take json command to change password

--- Modules/resetpassword/resetpassword_controller.php
+++ Modules/resetpassword/resetpassword_controller.php
@@ -16,7 +16,7 @@ defined('EMONCMS_EXEC') or die('Restricted access');
   
 function resetpassword_controller()
 {
-    global $user, $path, $session, $route ,$allowusersregister;
+    global $user, $path, $session, $route;
 
     $result = false;
 
@@ -29,24 +29,7 @@ function resetpassword_controller()
     // JSON API
     if ($route->format == 'json')
     {
-        // Core session
-        if ($route->action == 'login' && !$session['read']) $result = $user->login(post('username'),post('password'),post('rememberme'));
-        if ($route->action == 'register' && $allowusersregister) $result = $user->register(post('username'),post('password'),post('email'));
-        if ($route->action == 'logout' && $session['read']) $user->logout();
 
-        if ($route->action == ' changeusername' && $session['write']) $result = $user->change_username($session['userid'],get('username'));
-        if ($route->action == 'changeemail' && $session['write']) $result = $user->change_email($session['userid'],get('email'));
-        if ($route->action == 'changepassword' && $session['write']) $result = $user->change_password($session['userid'],get('old'),get('new'));
-
-        // Apikey
-        if ($route->action == 'newapikeyread' && $session['write']) $result = $user->new_apikey_read($session['userid']);
-        if ($route->action == 'newapikeywrite' && $session['write']) $result = $user->new_apikey_write($session['userid']);
-
-        // Get and set - user by profile client
-        if ($route->action == 'get' && $session['write']) $result = $user->get($session['userid']);
-        if ($route->action == 'set' && $session['write']) $result = $user->set($session['userid'],json_decode(get('data')));
-
-        if ($route->action == 'timezone' && $session['read']) $result = $user->get_timezone($session['userid']);
     }
 
     return array('content'=>$result);
