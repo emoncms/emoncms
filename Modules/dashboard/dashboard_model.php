1c51dd8f892cf411a5f2ba35ca67a0b62db723b7
PlaneteDomo <contact@planete-domotique.com>
2013-05-11 23:30:32 +0200

    Use mysqli->real_escape_string instead of preg_replace for dashboard content to allow °,% in units

--- Modules/dashboard/dashboard_model.php
+++ Modules/dashboard/dashboard_model.php
@@ -87,7 +87,9 @@ class Dashboard
       $userid = (int) $userid;
       $id = (int) $id;
       $height = (int) $height;
-      $content = preg_replace('/[^\w\s-.#<>?",;:=&\/%]/','',$content);
+      $content = $this->mysqli->real_escape_string($content);
+      
+      //echo $content;
 
       $result = $this->mysqli->query("SELECT * FROM dashboard WHERE userid = '$userid' AND id='$id'");
       $row = $result->fetch_array();
