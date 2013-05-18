69c643ad8c63e4a0eba3db3b0091e08d1b329aa8
Erik Karlsson <erik@intomethod.com>
2013-05-19 00:19:14 +0200

    hopefully last commit for this feature..

--- route.php
+++ route.php
@@ -29,8 +29,30 @@ class Route
 
     public function decode($q)
     {
-        // filter out the applications root to prevent invalid route-parsing
-        $q = str_replace(dirname(server('SCRIPT_NAME')), '', $q);
+        // filter out the applications relative root
+        
+        // If we're running in a subdirectory "emoncms", $q would look like '/emoncms/user/view' instead or just 'user/view'
+        // for the example of viewing a users profile. We need to remove the first directory to get the "clean" routing path
+        // within the application no matter at which path it's hosted.
+        
+        // First get the absolute physical path
+        // Example running at root: '/var/www' or subdirectory: '/var/www/emoncms'
+        $absolutePath = realpath(dirname(__FILE__));    
+        
+        // Next up, we need to find the relative path to the www root and remove everything except the part we will use to route
+        // for example this will perform the following:
+        // Running at root: str_replace('/var/www', '', '/var/www') => ''
+        // Running at subdirectory: str_replace('/var/www', '', '/var/www/emoncms') => '/emoncms'
+        $relativeApplicationPath = str_replace($_SERVER['DOCUMENT_ROOT'], '', $absolutePath);
+        
+        // Next up we will need to remove the '/emoncms' from the route path '/emoncms/user/view'
+        // str_replace('/emoncms', '', '/emoncms/user/view') => '/user/view'
+        // running at root path it will just perform nothing: str_replace('', '', '/emoncms/user/view') so it can be skipped
+        if ( !empty($relativeApplicationPath) ) {
+            $q = str_replace($relativeApplicationPath, '', $q);
+        }
+        
+        // trim slashes: '/user/view' => 'user/view'
         $q = trim($q, '/');
 	
         // filter out all except a-z and / .
