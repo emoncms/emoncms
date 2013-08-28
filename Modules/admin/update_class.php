d11927fdcea9aae423dd6b5e54c773716e950344
trystanlea <trystan.lea@gmail.com>
2013-08-28 18:03:17 +0100

    remove enum's as defined in enum.php

--- Modules/admin/update_class.php
+++ Modules/admin/update_class.php
@@ -1,18 +1,5 @@
 <?php
 
-class ProcessArg {
-  const VALUE = 0;
-  const INPUTID = 1;
-  const FEEDID = 2;
-  const NONE = 3;
-}
-
-class DataType {
-  const UNDEFINED = 0;
-  const REALTIME = 1;
-  const DAILY = 2;
-  const HISTOGRAM = 3;
-}
 
 class Update
 {
