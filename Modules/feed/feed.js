32b586e7db21eca913dce2a4ff116a3e91d359f1
TrystanLea <glyn.hudson@openenergymonitor.org>
2013-12-27 16:35:26 +0000

    Added method for requesting feeds by id

--- Modules/feed/feed.js
+++ Modules/feed/feed.js
@@ -1,11 +1,28 @@
 
 var feed = {
 
+  apikey: "",
+
   'list':function()
   {
     var result = {};
     $.ajax({ url: path+"feed/list.json", dataType: 'json', async: false, success: function(data) {result = data;} });
     return result;
+  },
+  
+  'list_by_id':function()
+  {
+    var feeds = {};
+    $.ajax({ url: path+"feed/list.json", dataType: 'json', async: false, success: function(data) {feeds = data;} });
+    
+    var tmp = {};
+    for (z in feeds)
+    {
+      tmp[feeds[z]['id']] = parseFloat(feeds[z]['value']);
+    }
+    var feeds = tmp;
+    
+    return feeds;
   },
 
   'set':function(id, fields)
@@ -25,9 +42,10 @@ var feed = {
   'get_data':function(feedid,start,end,dp)
   {
     var feedIn = [];
+    var apikeystr = ""; if (feed.apikey!="") apikeystr = "&apikey="+feed.apikey;
     $.ajax({                                      
       url: path+'feed/data.json',                         
-      data: "&apikey="+apikey+"&id="+feedid+"&start="+start+"&end="+end+"&dp="+dp,
+      data: apikeystr+"&id="+feedid+"&start="+start+"&end="+end+"&dp="+dp,
       dataType: 'json',
       async: false,                      
       success: function(data_in) { feedIn = data_in; } 
@@ -38,9 +56,10 @@ var feed = {
   'get_timestore_average':function(feedid,start,end,interval)
   {
     var feedIn = [];
+    var apikeystr = ""; if (feed.apikey!="") apikeystr = "&apikey="+feed.apikey;
     $.ajax({                                      
       url: path+'feed/timestoreaverage.json',                         
-      data: "&apikey="+apikey+"&id="+feedid+"&start="+start+"&end="+end+"&interval="+interval,
+      data: apikeystr+"&id="+feedid+"&start="+start+"&end="+end+"&interval="+interval,
       dataType: 'json',
       async: false,                      
       success: function(data_in) { feedIn = data_in; } 
@@ -51,9 +70,10 @@ var feed = {
   'get_kwhatpowers':function(feedid,points)
   {
     var feedIn = [];
+    var apikeystr = ""; if (feed.apikey!="") apikeystr = "&apikey="+feed.apikey;
     $.ajax({                                      
       url: path+'feed/kwhatpowers.json',                         
-      data: "&apikey="+apikey+"&id="+feedid+"&points="+JSON.stringify(points),
+      data: apikeystr+"&id="+feedid+"&points="+JSON.stringify(points),
       dataType: 'json',
       async: false,                      
       success: function(data_in) { feedIn = data_in; } 
@@ -64,9 +84,10 @@ var feed = {
   'histogram':function(feedid,start,end)
   {
     var feedIn = [];
+    var apikeystr = ""; if (feed.apikey!="") apikeystr = "&apikey="+feed.apikey;
     $.ajax({                                      
       url: path+'feed/histogram.json',                         
-      data: "&apikey="+apikey+"&id="+feedid+"&start="+start+"&end="+end+"&res=1",
+      data: apikeystr+"&id="+feedid+"&start="+start+"&end="+end+"&res=1",
       dataType: 'json',
       async: false,                      
       success: function(data_in) { feedIn = data_in; } 
