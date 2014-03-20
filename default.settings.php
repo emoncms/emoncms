b7b52b39e1791390120bb32f4554c1488d1ba89a
dogmatic69 <dogmatic69@gmail.com>
2014-03-20 23:48:49 +0000

    new config file

--- default.settings.php
+++ default.settings.php
@@ -1,95 +1,97 @@
 <?php
+/**
+ * Debug setting
+ */
+Configure::write('debug', 0);
 
-    /*
-
-    Database connection settings
-
-    */
-
-    $username = "";
-    $password = "";
-    $server   = "localhost";
-    $database = "";
-
-    $redis_enabled = true;
+/**
+ * Database Configuration
+ */
+Configure::write('DB_CONFIG', array(
+    'username' => null,
+    'password' => null,
+    'server' => 'localhost',
+    'database' => null,
     
-    $feed_settings = array(
+    
+    'dbtest' => true, // Skip database setup test - set to false once database has been setup.
+));
 
-        'creatable_engines'=>array('MYSQL','TIMESTORE','PHPTIMESERIES','GRAPHITE','PHPTIMESTORE'),
+/**
+ * redis configuration
+ */
+Configure::write('Redis', array(
+    'host' => '127.0.0.1',
+    'enabled' => true,
+));
 
-        'timestore'=>array(
-            'adminkey'=>""
-        ),
+/**
+ * SMTP configuration
+ */
+Configure::write('Smtp', array(
+    'host' => '',
+    'port' => 26,
+    'username' => '',
+    'password' => '',
+    'from' => array('email' => 'name'),
+));
 
-        'graphite'=>array(
-            'port'=>0,
-            'host'=>0
-        ),
+/**
+ * EmonCMS Configuration
+ */
+Configure::write('EmonCMS', array(
+    'theme' => 'default',
+    'enable_password_reset' => false,
+    'max_node_id_limit' => 32,
+    'Auth' => array(
+        // Default controller and action if none are specified and user is anonymous
+        'default_controller' => 'user',
+        'default_action' => 'login',
+
+        // Default controller and action if none are specified and user is logged in
+        'default_controller_auth' => 'user',
+        'default_action_auth' => 'view',
         
-        // The default data directory is /var/lib/phpfiwa,phpfina,phptimeseries on windows or shared hosting you will likely need to specify a different data directory.
-        // Make sure that emoncms has write permission's to the datadirectory folders
-        
-        'phpfiwa'=>array(
-            //'datadir'=>'/home/username/emoncmsdata/phpfiwa/'
-        ),
-        'phpfina'=>array(
-            //'datadir'=>'/home/username/emoncmsdata/phpfina/'
-        ),
-        'phptimeseries'=>array(
-            //'datadir'=>'/home/username/emoncmsdata/phptimeseries/'
-        )
-    );
+        'allowusersregister' => true,
+        'enable_rememberme' => true,
+    ),
+    'Profile' => array(
+        'public_profile_enabled' => true,
+        'public_profile_controller' => 'dashboard',
+        'public_profile_action' => 'view',
+    ),
+));
+
+/**
+ * Security related settings
+ */
+Configure::write('Security', array(
+    'salt' => 'random chars here, any length',
+));
+
+/**
+ * Feed Configuration
+ */
+Configure::write('Feed', array(
+    'engines' => array('MYSQL', 'TIMESTORE', 'PHPTIMESERIES', 'GRAPHITE', 'PHPTIMESTORE'),
+    'timestre' => array(
+        'adminkey' => ''
+    ),
+    'graphite' => array(
+        'port' => 0,
+        'host' => 0
+    ),
     
-    // (OPTIONAL) Used by password reset feature
-    $smtp_email_settings = array(
-      'host'=>"",
-      'username'=>"",
-      'password'=>"",
-      'from'=>array('an email address' => 'an email tag name')
-    );
-
-    $enable_password_reset = false;
+    // The default data directory is /var/lib/phpfiwa,phpfina,phptimeseries on windows or shared hosting you will likely need to specify a different data directory.
+    // Make sure that emoncms has write permission's to the datadirectory folders
     
-    // Checks for limiting garbage data?
-    $max_node_id_limit = 32;
-
-    /*
-
-    Default router settings - in absence of stated path
-
-    */
-
-    // Default controller and action if none are specified and user is anonymous
-    $default_controller = "user";
-    $default_action = "login";
-
-    // Default controller and action if none are specified and user is logged in
-    $default_controller_auth = "user";
-    $default_action_auth = "view";
-
-    // Public profile functionality
-    $public_profile_enabled = TRUE;
-    $public_profile_controller = "dashboard";
-    $public_profile_action = "view";
-
-    /*
-
-    Other
-
-    */
-
-    // Theme location
-    $theme = "basic";
-
-    // Error processing
-    $display_errors = TRUE;
-
-    // Allow user register in emoncms
-    $allowusersregister = TRUE;
-
-    // Enable remember me feature - needs more testing
-    $enable_rememberme = TRUE;
-
-    // Skip database setup test - set to false once database has been setup.
-    $dbtest = TRUE;
-
+    'phpfiwa' => array(
+        //'datadir'=> '/home/username/emoncmsdata/phpfiwa/',
+    ),
+    'phpfina' => array(
+        //'datadir'=> '/home/username/emoncmsdata/phpfina/',
+    ),
+    'phptimeseries' => array(
+        //'datadir' => '/home/username/emoncmsdata/phptimeseries/',
+    )
+));
