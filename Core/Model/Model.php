7d432574c9262ae747326d53ff782569e87eb768
dogmatic69 <dogmatic69@gmail.com>
2014-03-23 21:38:12 +0000

    fixing all the model issues

--- Core/Model/Model.php
+++ Core/Model/Model.php
@@ -8,86 +8,23 @@ class Model
 
 	public $useTable = null;
 
-	public $useDbConfig = null;
+	public $useDbConfig = 'default';
 
 	protected $_queryLog = array();
 
-	public function __construct($config) 
-	{
-		$this->useDbConfig = Configure::read('DB_CONFIG.database');
-	}
-
-	public function lastInsertId() 
-	{
-		return $this->id;
-	}
 /**
- * Check if the give record exists
+ * constructor
  *
- * @param integer $id the record id to check
+ * @param array $configuration
  *
- * @return boolean
+ * @return Model
  */
-	public function exists($id = null) 
+	public function __construct(array $config = array()) 
 	{
-		if ($id === null) 
+		if (defined('EMON_TEST_ENV') && EMON_TEST_ENV) 
 		{
-			$id = $this->id;
+			$this->useDbConfig = 'test';
 		}
-
-		$sql = sprintf('SELECT COUNT(*) as `count` FROM `%s` WHERE `%s` = :id', $this->useTable, $this->primaryKey);
-		return $this->field('count', $sql, array(
-			'id' => $id,
-		)) === 1;
-	}
-
-/**
- * Fetch the value of a single field
- *
- * @param string $field the field name to fetch
- * @param string $query the query to select the field
- *
- * @return string|null
- */
-	public function field($field, $query, $values = array()) 
-	{
-		$Statement = $this->query($query, $values);
-		if (!$Statement instanceof PDOStatement || !$Statement->rowCount()) 
-		{
-			return null;
-		}
-		
-		return $Statement->fetchColumn();
-	}	
-
-/**
- * Fetch a row / array of data
- *
- * @param string $query the query to select the field
- *
- * @return string|null
- */
-	public function row($query, array $values = array()) 
-	{
-		return (array)current($this->rows($query, $values));
-	}
-
-/**
- * Fetch a row / array of data
- *
- * @param string $query the query to select the field
- *
- * @return string|null
- */
-	public function rows($query, array $values = array()) 
-	{
-		$Statement = $this->query($query, $values);
-		if (!$Statement->rowCount()) 
-		{
-			return array();
-		}
-		$rows = $Statement->fetchAll(PDO::FETCH_ASSOC);
-		return $rows;
 	}
 
 /**
@@ -100,6 +37,99 @@ class Model
 		$this->id = null;
 	}
 
+/**
+ * get the last insert id
+ *
+ * @return null|integer
+ */
+	public function lastInsertId() 
+	{
+		return $this->id;
+	}
+
+/**
+ * Check if the give record exists
+ *
+ * @param integer $id the record id to check
+ *
+ * @return boolean
+ */
+	public function exists($id = null) 
+	{
+		$sql = sprintf('SELECT COUNT(*) as `count` FROM `%s` WHERE `%s` = :id', $this->useTable, $this->primaryKey);
+		return (int)$this->field($sql, array(
+			'id' => (int)($id ?: $this->id),
+		)) === 1;
+	}
+
+/**
+ * Fetch the value of a single field
+ *
+ * Returns the first value of a result
+ *
+ * @param string $field the field name to fetch
+ * @param string $query the query to select the field
+ *
+ * @return string|null
+ */
+	public function field($query, $values = array()) 
+	{
+		$Statement = $this->_query($query, $values);
+		$Statement->execute();
+		if (!$Statement instanceof PDOStatement || !$Statement->rowCount()) 
+		{
+			return null;
+		}
+		
+		$return = $Statement->fetchColumn();
+		$Statement->closeCursor();
+		return $return;
+	}
+
+/**
+ * Fetch a row / array of data
+ *
+ * @param string $query the query to select the field
+ * @param array $values the values to be used in the query
+ *
+ * @return string|null
+ */
+	public function row($query, array $values = array()) 
+	{
+		return (array)current($this->rows($query, $values));
+	}
+
+/**
+ * Fetch a row / array of data
+ *
+ * @param string $query the query to select the field
+ * @param array $values the values to be used in the query
+ *
+ * @return string|null
+ */
+	public function rows($query, array $values = array()) 
+	{
+		$Statement = $this->_query($query, $values);
+		if (!$Statement->rowCount()) 
+		{
+			return array();
+		}
+		$rows = $Statement->fetchAll(PDO::FETCH_ASSOC);
+		$Statement->closeCursor();
+		return $rows;
+	}
+
+/**
+ * Save data to the db
+ *
+ * Records with primary key set will do an update, records with no primary key do an insert
+ *
+ * Returns the newly created / updated data set
+ *
+ * @param array $data the data to be saved
+ *
+ * @return array
+ */
 	public function save(array $data) 
 	{
 		if (!empty($data[$this->primaryKey]) && $this->exists($data[$this->primaryKey]))
@@ -111,7 +141,8 @@ class Model
 			$Statement = $this->_insert($data);
 		}
 
-		$this->id = $this->_pdo()->lastInsertId();
+		$this->id = !empty($data['id']) ? $data['id'] : $this->_pdo()->lastInsertId();
+		$Statement->closeCursor();
 
 		return $this->_findById();
 
@@ -139,18 +170,36 @@ class Model
 		return $saved;
 	}
 
+/**
+ * Update a record
+ *
+ * @param array $data the data to update
+ *
+ * @return PDOStatement
+ */
 	protected function _update(array $data) 
 	{
 		$set = array();
 		foreach ($data as $key => $value) 
 		{
+			if ($key == $this->primaryKey) 
+			{
+				continue;
+			}
 			$set[] = sprintf('`%s` = :%s', $key, $key);
 		}
 
-		$sql = sprintf('UPDATE `%s` SET %s WHERE :primaryKey = :id', $this->useTable, implode(', ', $set));
-		return $this->query($sql, $data);
+		$sql = sprintf('UPDATE `%s` SET %s WHERE `%s` = :id', $this->useTable, implode(', ', $set), $this->primaryKey);
+		return $this->_query($sql, $data);
 	}
 
+/**
+ * insert a record
+ *
+ * @param array $data the data to inser
+ *
+ * @return PDOStatement
+ */
 	protected function _insert(array $data)
 	{
 		$fields = array_keys($data);
@@ -160,9 +209,16 @@ class Model
 			$value = ':' . $value;
 		}
 		$sql = 'INSERT INTO `%s` (`%s`) VALUES (%s)';
-		return $this->query(sprintf($sql, $this->useTable, implode('`, `', $fields), implode(', ', $values)), $data);
+		return $this->_query(sprintf($sql, $this->useTable, implode('`, `', $fields), implode(', ', $values)), $data);
 	}
 
+/**
+ * find a record by the primary key
+ *
+ * @param integer $id the id to search for
+ *
+ * @return array
+ */
 	protected function _findById($id = null) 
 	{
 		if ($id === null) 
@@ -173,6 +229,25 @@ class Model
 		return $this->row(sprintf('SELECT * FROM `%s` WHERE `%s` = :id', $this->useTable, $this->primaryKey), array(
 			'id' => $id,
 		));
+	}
+
+/**
+ * Wrapper method for doing queries on the database
+ *
+ * Returns an associate array of data for the selected query
+ *
+ * @param string $sql the query to run
+ * @param array $value parameters to bind to the query
+ *
+ * @return array
+ */
+	public function query($sql, array $values) 
+	{
+		$Statement = self::_query($sql, $values);
+		$results = $Statement->fetchAll(PDO::FETCH_ASSOC);
+		$Statement->closeCursor();
+
+		return $results;
 	}
 
 /**
@@ -187,33 +262,23 @@ class Model
  *
  * @return PDOStatement
  */
-	public function query($sql, array $values = array()) 
+	protected function _query($sql, array $values = array()) 
 	{
 		$time = microtime(true);
-		try 
+		if (!empty($values)) 
 		{
-			if (!empty($values)) 
+			$Statement = $this->_pdo()->prepare($sql);
+			$bind = array();
+			foreach ($values as $k => $v) 
 			{
-				$Statement = $this->_pdo()->prepare($sql);
-				$bind = array();
-				foreach ($values as $k => $v) 
-				{
-					$bind[':' . $k] = $v;
-				}
-				$Statement->execute($bind);
-			} 
-			else 
-			{
-				$Statement = $this->_pdo()->query($sql);
+				$bind[':' . $k] = $v;
 			}
+			$Statement->execute($bind);
 		} 
-		catch (PDOException $e) 
+		else 
 		{
-			$error = $e->getMessage();
-			pr($error);
+			$Statement = $this->_pdo()->query($sql);
 		}
-
-		$Statement->closeCursor();
 
 		$this->_queryLog[] = array(
 			'query' => $sql,
@@ -222,19 +287,26 @@ class Model
 			'error' => isset($error) ? $error : null,
 		);
 
-		if (isset($e) || !$Statement instanceof PDOStatement) 
-		{
-			return null;
-		}
-
 		return $Statement;
 	}
 
+/**
+ * Get the query log
+ *
+ * Returns a log of queries that have been run, with data such as affected rows, time taken etc.
+ *
+ * @return array
+ */
 	public function queryLog() 
 	{
 		return $this->_queryLog;
 	}
 
+/**
+ * Get the PDO instance for doing queries
+ *
+ * @return PDO
+ */
 	protected function _pdo() 
 	{
 		if (empty($this->_pdo)) 
@@ -243,4 +315,5 @@ class Model
 		}
 		return $this->_pdo;
 	}
+
 }
