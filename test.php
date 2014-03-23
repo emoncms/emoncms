58864c5cc1671a04c576b993bdb2c3573b3ffdd3
dogmatic69 <dogmatic69@gmail.com>
2014-03-23 21:39:01 +0000

    make test work with basic fixtures in xml, fixtures are reloaded with each tests and the db is recreated at the start of every test session

--- test.php
+++ test.php
@@ -3,17 +3,24 @@ $root = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 
 $ltime = microtime(true);
 
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+mb_language('uni');
+mb_regex_encoding('UTF-8');
+
 define('EMONCMS_EXEC', 1);
+define('EMON_TEST_ENV', 1);
 
 // 1) Load settings and core scripts
-require $root . 'process_settings.php';
-require $root . 'core.php';
-require $root . 'route.php';
-require $root . 'locale.php';
-require CORE . 'Model' . DS . 'ConnectionManager.php';
-require CORE . 'Model' . DS . 'Model.php';
+require_once $root . 'process_settings.php';
+require_once $root . 'core.php';
+require_once $root . 'route.php';
+require_once $root . 'locale.php';
 
-require CORE . 'TestSuite' . DS . 'EmonTestCase.php';
+require_once CORE . 'Model' . DS . 'ConnectionManager.php';
+require_once CORE . 'Model' . DS . 'Model.php';
+require_once CORE . 'TestSuite' . DS . 'EmonTestCase.php';
 
 /**
  * AllEmonTest
@@ -31,7 +38,8 @@ class AllEmonTest extends PHPUnit_Framework_TestSuite {
 		echo sprintf("EmonCMS Test Suite v%s\n", Configure::read('EmonCMS.version'));
 		echo implode("\n", array(
 			'==============================',
-			sprintf('Base Path: %s', CORE),
+			sprintf('Base Path: %s', ROOT),
+			sprintf('Core Path: %s', CORE),
 			'==============================',
 			'',
 			''
@@ -43,6 +51,14 @@ class AllEmonTest extends PHPUnit_Framework_TestSuite {
 	    $files = new RegexIterator($ite, '/.*Test\.php/', RegexIterator::GET_MATCH);
 	    $fileList = array();
 	    foreach($files as $file) {
+	    	foreach ($file as $k => $v) {
+	    		if (strstr($v, 'StringTest.php') || strstr($v, 'HashTest.php')) {
+	    			unset($file[$k]);
+	    		}
+	    	}
+	    	if (empty($file)) {
+	    		continue;
+	    	}
 	        $fileList = array_merge($fileList, $file);
 	    }
 
