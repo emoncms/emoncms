b610619553de79d5b2b22aa81b15b8375f659053
arjenhiemstra <arjenh@me.com>
2014-03-24 11:30:17 +0100

    Update feedvalue_render.js
    Sometimes it is needed to be able to control the number of decimal places in a feed value. This update makes it possible

--- Modules/dashboard/Views/js/widgets/feedvalue/feedvalue_render.js
+++ Modules/dashboard/Views/js/widgets/feedvalue/feedvalue_render.js
@@ -18,10 +18,10 @@ function feedvalue_widgetlist()
     {
       "offsetx":-40,"offsety":-30,"width":80,"height":60,
       "menu":"Widgets",
-      "options":["feedname","units"],
-      "optionstype":["feed","value"],
-      "optionsname":[_Tr("Feed"),_Tr("Units")],
-      "optionshint":[_Tr("Feed value"),_Tr("Units to show")]
+      "options":["feedname","units","decimals"],
+      "optionstype":["feed","value","decimals"],
+      "optionsname":[_Tr("Feed"),_Tr("Units"),_Tr("Decimals")],
+      "optionshint":[_Tr("Feed value"),_Tr("Units to show"),_Tr("Decimals to show (-1 for automatic)")]
     }
   }
   return widgets;
@@ -41,23 +41,33 @@ function feedvalue_draw()
 
     var units = $(this).attr("units");
     var val = assoc[feed];
+    var decimals = $(this).attr("decimals");
 
     if (feed==undefined) val = 0;
     if (units==undefined) units = '';
     if (val==undefined) val = 0;
-
+    if (decimals==undefined) decimals = -1;
+    
     if (isNaN(val))  val = 0;
+    
+    if (decimals==-1)
+    {
 
-    else if (val>=100)
-        val = (val*1).toFixed(0);
-    else if (val>=10)
-        val = (val*1).toFixed(1);
-    else if (val<=-100)
-        val = (val*1).toFixed(0);
-    else if (val<=-10)
-        val = (val*1).toFixed(1);
-    else
-        val = (val*1).toFixed(2);
+      if (val>=100)
+          val = (val*1).toFixed(0);
+      else if (val>=10)
+          val = (val*1).toFixed(1);
+      else if (val<=-100)
+          val = (val*1).toFixed(0);
+      else if (val<=-10)
+          val = (val*1).toFixed(1);
+      else
+          val = (val*1).toFixed(2);
+    }
+    else 
+    {
+      val = val.toFixed(decimals);
+    }
 
     $(this).html(val+units);
   });
