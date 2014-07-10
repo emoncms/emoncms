3b065e4f261600dd54e353282d6ba63fcc1ebf69
TrystanLea <glyn.hudson@openenergymonitor.org>
2014-07-10 13:05:07 +0100

    removed non write buffering code

--- Modules/input/input_menu.php
+++ Modules/input/input_menu.php
@@ -4,4 +4,4 @@
     bindtextdomain($domain, "Modules/input/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
 
-    $menu_left[] = array('name'=> dgettext($domain, "Input"), 'path'=>"input/node" , 'session'=>"write", 'order' => 1 );
+    $menu_left[] = array('name'=> dgettext($domain, "Input"), 'path'=>"input/view" , 'session'=>"write", 'order' => 1 );
--- scripts/readme.md
+++
@@ -1,3 +0,0 @@
-# input_queue_processor.php
-
-To be used in association with que_input_controller.php see Modules > Input > readme.md
