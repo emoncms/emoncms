1fc0ebe21b3a4d18e24d446679fc149d522c9e85
TrystanLea <glyn.hudson@openenergymonitor.org>
2014-07-10 12:58:08 +0100

    Removal of visualisations that dont work with current implementation of buffered writing - to be reintroduced in future

--- Modules/vis/vis_controller.php
+++ Modules/vis/vis_controller.php
@@ -15,12 +15,12 @@
 
   function vis_controller()
   {
-    global $mysqli, $redis, $session, $route, $user, $timestore_adminkey;
+    global $mysqli, $redis, $session, $route, $user, $settings;
 
     $result = false;
 
     require "Modules/feed/feed_model.php";
-    $feed = new Feed($mysqli,$redis, $timestore_adminkey);
+    $feed = new Feed($mysqli,$redis,$settings);
 
     require "Modules/vis/multigraph_model.php";
     $multigraph = new Multigraph($mysqli);
@@ -38,26 +38,18 @@
     */
 
     $visualisations = array(
+        'multigraph' => array ('action'=>'multigraph', 'options'=>array(array('mid',7))),
         'realtime' => array('options'=>array(array('feedid',1))),
         // Hex colour EDC240 is the default color for flot. since we want existing setups to not change, we set the default value to it manually now,
         'rawdata'=> array('options'=>array(array('feedid',1),array('fill',7,0),array('units',5,'W'),array('colour',5,'EDC240'))),
         'bargraph'=> array('options'=>array(array('feedid',2),array('colour',5,'EDC240'))),
-        'timestoredaily'=> array('options'=>array(array('feedid',1),array('units',5,'kWh'))),
+        'simplezoom'=> array('options'=>array(array('power',1),array('kwhd',2))),
+        
         'smoothie'=> array('options'=>array(array('feedid',1),array('ufac',6))),
-        'histgraph'=> array('options'=>array(array('feedid',3),array('barwidth',7,50),array('start',7,0),array('end',7,0))),
-        //'dailyhistogram'=> array('options'=>array(array('feedid',3))),
         'zoom'=> array('options'=>array(array('power',1),array('kwhd',2),array('currency',5,'&pound;'),array('currency_after_val', 7, 0), array('pricekwh',6,0.14))),
-        //'comparison'=> array('options'=>array(array('feedid',3))),
         'stacked'=> array('options'=>array(array('bottom',2),array('top',2))),
         'stackedsolar'=> array('options'=>array(array('solar',2),array('consumption',2))),
-        'threshold'=> array('options'=>array(array('feedid',3),array('thresholdA',6,500),array('thresholdB',6,2500))),
-        'simplezoom'=> array('options'=>array(array('power',1),array('kwhd',2))),
-        'orderbars'=> array('options'=>array(array('feedid',2))),
-        'orderthreshold'=> array('options'=>array(array('feedid',3),array('power',1),array('thresholdA',6,500),array('thresholdB',6,2500))),
-        'editrealtime'=> array('options'=>array(array('feedid',1))),
-        'editdaily'=> array('options'=>array(array('feedid',2))),
-        'multigraph' => array ('action'=>'multigraph', 'options'=>array(array('mid',7)) ),
-        'compare' => array ('action'=>'compare', 'options'=>array(array('powerx',1),array('powery',1)))
+        'orderbars'=> array('options'=>array(array('feedid',2)))
     );
 
     $write_apikey = ""; $read_apikey = "";
