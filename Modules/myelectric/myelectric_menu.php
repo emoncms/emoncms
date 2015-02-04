fe385947305a0844aa2adc6944221c56a91833bd
Gabriele Lauricella <gab.lau@gmail.com>
2015-02-04 20:23:07 +0100

    Translation "My electric" Module
    Full translation in Italian Language
    - Fix user_model.php to take the language on apykey_session
    - Revert module name in english see:
    http://openenergymonitor.org/emon/node/10127

--- Modules/myelectric/myelectric_menu.php
+++ Modules/myelectric/myelectric_menu.php
@@ -1,7 +1,11 @@
 <?php
 
     global $session, $user;
+    
+    $domain = "messages";
+    bindtextdomain($domain, "Modules/myelectric/locale");
+    bind_textdomain_codeset($domain, 'UTF-8');
   
     if ($session['write']) $apikey = "?apikey=".$user->get_apikey_write($session['userid']); else $apikey = "";
   
-    $menu_left[] = array('name'=>"Meine Elektrik", 'path'=>"myelectric".$apikey , 'session'=>"write", 'order' => -2 );
+    $menu_left[] = array('name'=> dgettext($domain, "My Electric"), 'path'=>"myelectric".$apikey , 'session'=>"write", 'order' => -2 );
--- Modules/user/user_model.php
+++ Modules/user/user_model.php
@@ -53,11 +53,11 @@ class User
             $session['write'] = 1;
             $session['admin'] = 0;
             $session['editmode'] = TRUE;
-            $session['lang'] = "en";
+            $session['lang'] = $this->redis->get("language:$apikey_in");
         }
         else
         {
-            $result = $this->mysqli->query("SELECT id FROM users WHERE apikey_write='$apikey_in'");
+            $result = $this->mysqli->query("SELECT id,language FROM users WHERE apikey_write='$apikey_in'");
             if ($result->num_rows == 1)
             {
                 $row = $result->fetch_array();
@@ -69,14 +69,15 @@ class User
                     $session['write'] = 1;
                     $session['admin'] = 0;
                     $session['editmode'] = TRUE;
-                    $session['lang'] = "en";
+                    $session['lang'] = $row['language'];
 
                     if ($this->redis) $this->redis->set("writeapikey:$apikey_in",$row['id']);
+                    if ($this->redis) $this->redis->set("language:$apikey_in",$row['language']);
                 }
             }
             else
             {
-            $result = $this->mysqli->query("SELECT id FROM users WHERE apikey_read='$apikey_in'");
+            $result = $this->mysqli->query("SELECT id,language FROM users WHERE apikey_read='$apikey_in'");
             if ($result->num_rows == 1)
             {
                 $row = $result->fetch_array();
@@ -88,7 +89,7 @@ class User
                     $session['write'] = 0;
                     $session['admin'] = 0;
                     $session['editmode'] = TRUE;
-                    $session['lang'] = "en";
+                    $session['lang'] = $row['language'];
                 }
             }
             }
