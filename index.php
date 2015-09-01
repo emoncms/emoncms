175a9257651b1df27bc1f623435435a997e252c1
chaveiro <nchaveiro@gmail.com>
2015-09-01 21:50:16 +0100

    EmonCMS | 9 RC | 2015.09.01
    Fixed API documentation.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     require "route.php";
     require "locale.php";
 
-    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9 RC | 2015.08.29";
+    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9 RC | 2015.09.01";
 
     $path = get_application_path();
     require "Lib/EmonLogger.php";
