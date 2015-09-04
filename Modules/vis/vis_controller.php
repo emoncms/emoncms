5bd8c87acb75435febafcba1135da37b8d2e15af
JSidrach <juan.sidrach@gmail.com>
2015-09-05 00:25:01 +0200

    Fix bug that broke regexp with underscores

--- Modules/vis/vis_controller.php
+++ Modules/vis/vis_controller.php
@@ -89,7 +89,7 @@
                             if (get($key)==true || get($key)==false)
                                 $array[$key] = get($key); else $array[$key] = $default;
                         else if ($type==5)
-                            $array[$key] = preg_replace('/[^\p{L}\p{N}\s£$€¥]/u','',get($key))?get($key):$default;
+                            $array[$key] = preg_replace('/[^\p{L}_\p{N}\s£$€¥]/u','',get($key))?get($key):$default;
                         else if ($type==6)
                             $array[$key] = str_replace(',', '.', floatval((get($key)?get($key):$default)));
                         else if ($type==7)
