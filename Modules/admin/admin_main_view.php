bdf74b7be5d0ea82e9402a2ac7047d9e2d8a895d
Rubén de Celis Hernández <ruter106@gmail.com>
2016-02-16 16:00:52 +0100

    Fixed gethostbyaddr(): Address is not a valid IPv4 or IPv6 address

--- Modules/admin/admin_main_view.php
+++ Modules/admin/admin_main_view.php
@@ -36,7 +36,7 @@
                  'mqtt_ip' => gethostbyname($mqtt_server['host']),
                  'mqtt_port' => $mqtt_server['port'],
 
-                 'hostbyaddress' => gethostbyaddr(gethostbyname($host)),
+                 'hostbyaddress' => @gethostbyaddr(gethostbyname($host)),
                  'http_proto' => $_SERVER['SERVER_PROTOCOL'],
                  'http_mode' => $_SERVER['GATEWAY_INTERFACE'],
                  'http_port' => $_SERVER['SERVER_PORT'],
