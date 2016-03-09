2261b2629eda1070ef1e945ff3fa07053b9524f8
Trystan Lea <trystan.lea@googlemail.com>
2016-03-09 06:56:36 +0000

    Set delta=0 so that it does not affect existing graphs

--- Modules/vis/vis_object.php
+++ Modules/vis/vis_object.php
@@ -65,7 +65,7 @@
             array('currency',_("currency"),5,'&pound;'),
             array('currency_after_val',_("currency_after_val"),7, 0),
             array('pricekwh',_("pricekwh"),6,0.14),
-            array('delta',_("delta"),7,1)
+            array('delta',_("delta"),7,0)
         )),
         
         //'comparison'=> array('options'=>array(array('feedid',3))),
@@ -74,13 +74,13 @@
             array('top',_("top"),0),
             array('colourt',_("colourt"),9,'7CC9FF'),
             array('colourb',_("colourb"),9,'0096FF'),
-            array('delta',_("delta"),7,1)
+            array('delta',_("delta"),7,0)
         )),
         
         'stackedsolar'=> array('label'=>_("StackedSolar"), 'options'=>array(
             array('solar',_("solar"),0),
             array('consumption',_("consumption"),0),
-            array('delta',_("delta"),7,1)
+            array('delta',_("delta"),7,0)
         )),
         
         'threshold'=> array('label'=>_("Threshold"), 'options'=>array(
@@ -92,12 +92,12 @@
         'simplezoom'=> array('label'=>_("SimpleZoom"), 'options'=>array(
             array('power',_("power"),1),
             array('kwhd',_("kwh"),0),
-            array('delta',_("delta"),7,1)
+            array('delta',_("delta"),7,0)
         )),
         
         'orderbars'=> array('label'=>_("OrderBars"), 'options'=>array(
             array('feedid',_("feed"),0),
-            array('delta',_("delta"),7,1)
+            array('delta',_("delta"),7,0)
         )),
         
         'orderthreshold'=> array('label'=>_("OrderThreshold"), 'options'=>array(
