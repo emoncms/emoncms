d5fe9a5743a7269381bd2e69554d349d2fd1e36d
Glyn Hudson <glynhudson@users.noreply.github.com>
2016-05-25 11:12:00 +0100

    bump version 9.7 | 2016.05.25

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     require "route.php";
     require "locale.php";
 
-    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9.6 | 2016.05.11";
+    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9.7 | 2016.05.25";
 
     $path = get_application_path();
     require "Lib/EmonLogger.php";
