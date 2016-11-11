7c7d9c978190b104bd41c22cd32cfface792d744
Ryan Brooks <ryanbrooksis@gmail.com>
2016-11-11 16:45:17 +0100

    fixes typo in settings processor

--- process_settings.php
+++ process_settings.php
@@ -122,7 +122,7 @@ if (isset($_ENV["MYSQL_PORT"]))     $port = $_ENV["MYSQL_PORT"];
 // create the array if it's not already been done
 if (!isset($redis_server)) $redis_server = array();
 
-if (isset($_ENV["REDIS_ENABLED"]))  $redis_enabled = $_ENV["REDIS_ENABLED"] === 'true' : false;
+if (isset($_ENV["REDIS_ENABLED"]))  $redis_enabled = $_ENV["REDIS_ENABLED"] === 'true';
 if (isset($_ENV["REDIS_HOST"]))     $redis_server['host'] = $_ENV["REDIS_HOST"];
 if (isset($_ENV["REDIS_PORT"]))     $redis_server['port'] = $_ENV["REDIS_PORT"];
 if (isset($_ENV["REDIS_AUTH"]))     $redis_server['auth'] = $_ENV["REDIS_AUTH"];
@@ -131,10 +131,10 @@ if (isset($_ENV["REDIS_PREFIX"]))   $redis_server['prefix'] = $_ENV["REDIS_PREFI
 //3 #### MQTT
 // create the array if it's not already been done
 if (!isset($mqtt_server)) $mqtt_server = array();
-if (isset($_ENV["MQTT_ENABLED"]))  $mqtt_enabled = $_ENV["MQTT_ENABLED"] === 'true' : false;
+if (isset($_ENV["MQTT_ENABLED"]))  $mqtt_enabled = $_ENV["MQTT_ENABLED"] === 'true';
 
-if (isset($_ENV["REDIS_HOST"]))     $redis_server['host'] = $_ENV["MQTT_HOST"];
-if (isset($_ENV["REDIS_HOST"]))     $redis_server['port'] = $_ENV["MQTT_PORT"];
-if (isset($_ENV["REDIS_HOST"]))     $redis_server['user'] = $_ENV["MQTT_USER"];
-if (isset($_ENV["REDIS_HOST"]))     $redis_server['password'] = $_ENV["MQTT_PASSWORD"];
-if (isset($_ENV["REDIS_HOST"]))     $redis_server['basetopic'] = $_ENV["MQTT_BASETOPIC"];
+if (isset($_ENV["MQTT_HOST"]))     $redis_server['host'] = $_ENV["MQTT_HOST"];
+if (isset($_ENV["MQTT_PORT"]))     $redis_server['port'] = $_ENV["MQTT_PORT"];
+if (isset($_ENV["MQTT_USER"]))     $redis_server['user'] = $_ENV["MQTT_USER"];
+if (isset($_ENV["MQTT_PASSWORD"]))     $redis_server['password'] = $_ENV["MQTT_PASSWORD"];
+if (isset($_ENV["MQTT_BASETOPIC"]))     $redis_server['basetopic'] = $_ENV["MQTT_BASETOPIC"];
