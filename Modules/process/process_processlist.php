52ec995676da519353f08d4f6b77dd9926d9eb93
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-02-28 20:13:39 +0000

    description for "Total pulse count to pulse increment"

--- Modules/process/process_processlist.php
+++ Modules/process/process_processlist.php
@@ -103,7 +103,7 @@ class Process_ProcessList
         $list[19] = array(_("Power gained to kWh/d"),ProcessArg::FEEDID,"power_acc_to_kwhd",1,DataType::DAILY,"Deleted",array(Engine::PHPTIMESERIES), 'desc'=>"");
 
         // - look into implementation that doesnt need to store the ref feed
-        $list[20] = array(_("Total pulse count to pulse increment"),ProcessArg::FEEDID,"pulse_diff",1,DataType::REALTIME,"Pulse",array(Engine::PHPFINA,Engine::PHPTIMESERIES), 'desc'=>"");
+        $list[20] = array(_("Total pulse count to pulse increment"),ProcessArg::FEEDID,"pulse_diff",1,DataType::REALTIME,"Pulse",array(Engine::PHPFINA,Engine::PHPTIMESERIES), 'desc'=>"Returns the number of pulses incremented since the last update for a input that is a cumulative pulse count. i.e If the input updates from 23400 to 23410 the result will be an incremenet of 10.");
 
         // fixed works now with redis - look into state implementation without feed
         $list[21] = array(_("kWh to Power"),ProcessArg::FEEDID,"kwh_to_power",1,DataType::REALTIME,"Power & Energy",array(Engine::PHPFIWA,Engine::PHPFINA,Engine::PHPTIMESERIES), 'requireredis'=>true, 'desc'=>"Convert accumulating kWh to instantaneous power");
