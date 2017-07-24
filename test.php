c3ab0e3e7ae590d7f97b4d1723290ad3960f69dd
chaveiro <nchaveiro@gmail.com>
2017-07-24 10:51:24 +0100

    Test PHP 5.3 - 7.x compatibility

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?php
 
+// PHP 5.3 - 7.x compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
+
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
