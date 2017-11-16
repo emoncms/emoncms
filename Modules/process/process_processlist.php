3a17a4f81c91bb5a78cf517470a5939d1b5c2916
cagabi <cagabi@lapiluka.org>
2017-11-16 14:54:49 +0000

    Data checker: new processes added to processlist in process module to limit the max and min value of an input

--- Modules/process/process_processlist.php
+++ Modules/process/process_processlist.php
@@ -54,6 +54,9 @@ class Process_ProcessList
         $list = array();
         // 0=>Name | 1=>Arg type | 2=>function | 3=>No. of datafields if creating feed | 4=>Datatype | 5=>Group | 6=>Engines | 'requireredis'=>true | 'desc'=>Description | 'internalerror'=>true | 'internalerror_reason'=>true
         $list[] = array(_("EXIT"), ProcessArg::NONE, "error_found", 0, DataType::UNDEFINED, "Hidden", 'desc'=>"<p>This was automaticaly added when a loop error was discovered on the processList or execution took too many steps to process.  Review the usage of GOTOs or decrease the number of items and delete this entry to resume execution.</p>", 'internalerror'=>true,'internalerror_reason'=>"HAS ERRORS",'internalerror_desc'=>'Processlist disabled due to errors found during execution.');
+        $list[] = array(_("Max value allowed"), ProcessArg::VALUE, "max_value_allowed", 0, DataType::UNDEFINED, "Limits", 'desc'=>"<p>If value is greater than <i>max value allowed</i> then the value passed to following process will be the <i>max value allowed</i></p>",'requireredis'=>false,'nochange'=>false);
+        $list[] = array(_("Min value allowed"), ProcessArg::VALUE, "min_value_allowed", 0, DataType::UNDEFINED, "Limits", 'desc'=>"<p>If value is lower than <i>min value allowed</i> then the value passed to following process will be the <i>min value allowed</i></p>",'requireredis'=>false,'nochange'=>false);
+        
         return $list;
     }
 
@@ -190,6 +193,17 @@ class Process_ProcessList
     public function allownegative($arg, $time, $value)
     {
         if ($value>0) $value = 0;
+        return $value;
+    }
+    
+    public function max_value_allowed($arg, $time, $value)
+    {
+        if ($value>$arg) $value = $arg;
+        return $value;
+    }
+    public function min_value_allowed($arg, $time, $value)
+    {
+        if ($value<$arg) $value = $arg;
         return $value;
     }
 
