be7c768b5d0f25959fd5aad3d4e8ae841dfb983d
Paul <pb66@users.noreply.github.com>
2017-11-22 23:34:48 +0000

    Change index.php for versioning

--- index.php
+++ index.php
@@ -20,8 +20,13 @@
     require "route.php";
     require "param.php";
     require "locale.php";
+    require "module.json";
 
-    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9.8.18 | 2017.11.20";
+    $json = json_decode(file_get_contents("module.json"));  // Get JSON version information
+    $jsonVersion = $json->{'version'};
+    $jsonDate = $json->{'date'};
+
+    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "$jsonVersion | $jsonDate";
 
     $path = get_application_path();
     require "Lib/EmonLogger.php";
