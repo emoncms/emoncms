1e682836003411c267f7905b44922cb09331de71
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-12-08 22:29:16 +0000

    update version number

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     require "param.php";
     require "locale.php";
 
-    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9.8.24 | 2017.11.27";
+    $emoncms_version = ($feed_settings['redisbuffer']['enabled'] ? "low-write " : "") . "9.8.25 | 2017.12.08";
 
     $path = get_application_path();
     require "Lib/EmonLogger.php";
