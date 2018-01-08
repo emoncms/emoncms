c42b615c92b24d2ac1f13bb314630865c297ee0c
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-01-08 12:04:41 +0000

    fix 'not null' db schema column creation

--- Lib/dbschemasetup.php
+++ Lib/dbschemasetup.php
@@ -30,8 +30,9 @@ function db_schema_setup($mysqli, $schema, $apply)
             //-----------------------------------------------------
             while ($field = key($schema[$table]))
             { 
+                $null = false;
                 $type = $schema[$table][$field]['type'];
-                if (isset($schema[$table][$field]['Null'])) $null = $schema[$table][$field]['Null']; else $null = "YES";
+                if (isset($schema[$table][$field]['Null']) && $schema[$table][$field]['Null']==true) $null = true;
                 if (isset($schema[$table][$field]['Key'])) $key = $schema[$table][$field]['Key']; else $key = null;
                 if (isset($schema[$table][$field]['default'])) $default = $schema[$table][$field]['default']; else unset($default);
                 if (isset($schema[$table][$field]['Extra'])) $extra = $schema[$table][$field]['Extra']; else $extra = null;
@@ -41,7 +42,7 @@ function db_schema_setup($mysqli, $schema, $apply)
                 if ($result->num_rows==0)
                 {
                     $query = "ALTER TABLE `$table` ADD `$field` $type";
-                    if ($null) $query .= " NOT NULL";
+                    if (!$null) $query .= " NOT NULL";
                     if (isset($default)) $query .= " DEFAULT '$default'";
                     $operations[] = $query;
                     if ($apply) $mysqli->query($query);
