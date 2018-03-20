51822b437be686d3e28a7e7a7c80af4b214eb4dd
borpin <brian.orpin@gmail.com>
2018-03-20 21:23:28 +0000

    Include example of using Authorization: Bearer

--- Modules/feed/Views/feedapi_view.php
+++ Modules/feed/Views/feedapi_view.php
@@ -22,7 +22,7 @@
 <p><?php echo _('If you want to call any of the following actions when your not logged in you have this options to authenticate with the API key:'); ?></p>
 <ul><li><?php echo _('Append on the URL of your request: &apikey=APIKEY'); ?></li>
 <li><?php echo _('Use POST parameter: "apikey=APIKEY"'); ?></li>
-<li><?php echo _('Add the HTTP header: "Authorization: Bearer APIKEY"'); ?></li></ul>
+<li><?php echo _('Add the HTTP header: "Authorization: Bearer APIKEY" e.g. curl http://127.0.0.1/emoncms/feed/value.json?id=1 -H "Authorization: Bearer APIKEY"');?></li></ul>
 <p><b><?php echo _('Read only:'); ?></b><br>
 <input type="text" style="width:255px" readonly="readonly" value="<?php echo $user->get_apikey_read($session['userid']); ?>" />
 </p>
