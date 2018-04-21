2f1693bbea69af015aefe294dd85db2d697d70af
brucemiranda <brucemiranda@gmail.com>
2018-04-21 13:43:43 +0100

    Update install.sh
    Amended the path of match the Linux install instructions which now include html in the install path. Also changed the script to take back ups of all file rather than deleting them.

--- scripts/logger/install.sh
+++ scripts/logger/install.sh
@@ -3,23 +3,23 @@
 echo "Installing rc.local to create"
 echo "directory for emoncms log files"
 echo "after a reboot..."
-sudo rm /etc/rc.local
-sudo ln -s /var/www/emoncms/scripts/logger/rc.local /etc/rc.local
+sudo mv /etc/rc.local /etc/old.rc.local
+sudo ln -s /var/www/html/emoncms/scripts/logger/rc.local /etc/rc.local
 sudo chmod a+x /etc/rc.local
 echo ""
 echo "Completed"
 echo "Now setting up Logrotate..."
-sudo mv /etc/logrotate.conf /etc/logrotate.old
-sudo chown root /var/www/emoncms/scripts/logger/logrotate.conf
-sudo ln -s /var/www/emoncms/scripts/logger/logrotate.conf /etc/logrotate.conf
-sudo chmod a+x /var/www/emoncms/scripts/logger/logrotate
-sudo ln -s /var/www/emoncms/scripts/logger/logrotate /etc/cron.hourly/logrotate
-sudo rm /etc/cron.daily/logrotate
+sudo mv /etc/logrotate.conf /etc/old.logrotate
+sudo chown root /var/www/html/emoncms/scripts/logger/logrotate.conf
+sudo ln -s /var/www/html/emoncms/scripts/logger/logrotate.conf /etc/logrotate.conf
+sudo chmod a+x /var/www/html/emoncms/scripts/logger/logrotate
+sudo ln -s /var/www/html/emoncms/scripts/logger/logrotate /etc/cron.hourly/logrotate
+sudo mv /etc/cron.daily/logrotate /etc/cron.daily/old.logrotate 
 sudo touch /etc/cron.daily/logrotate
 
 echo "setup logrotate state & logfile in /var/log/logrotate"
 sudo mkdir /var/log/logrotate
-chown -R pi:pi /var/log/logrotate
+chown -R root:adm /var/log/logrotate
 
 echo ""
 echo "Completed"
