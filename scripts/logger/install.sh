3a9cf14c0a82c090200601cfaf50dae52fc00d03
brucemiranda <brucemiranda@gmail.com>
2018-04-22 14:26:08 +0100

    Update install.sh
    Updated the paths to include the /var/www/html/ as per the linux instructions. Also now replaced all the rm with mv so that a back up is kept of everything. Also fixed the pi:pi to root:adm

--- scripts/logger/install.sh
+++ scripts/logger/install.sh
@@ -3,23 +3,32 @@
 echo "Installing rc.local to create"
 echo "directory for emoncms log files"
 echo "after a reboot..."
-sudo rm /etc/rc.local
-sudo ln -s /var/www/emoncms/scripts/logger/rc.local /etc/rc.local
+echo "Backing up old rc.local..."
+sudo mv /etc/rc.local /var/www/html/emoncms/scripts/logger/rc.local.old
+echo "Linked to new rc.local..."
+sudo ln -s /var/www/html/emoncms/scripts/logger/rc.local /etc/rc.local
 sudo chmod a+x /etc/rc.local
 echo ""
 echo "Completed"
+
 echo "Now setting up Logrotate..."
-sudo mv /etc/logrotate.conf /etc/logrotate.old
-sudo chown root /var/www/emoncms/scripts/logger/logrotate.conf
-sudo ln -s /var/www/emoncms/scripts/logger/logrotate.conf /etc/logrotate.conf
-sudo chmod a+x /var/www/emoncms/scripts/logger/logrotate
-sudo ln -s /var/www/emoncms/scripts/logger/logrotate /etc/cron.hourly/logrotate
-sudo rm /etc/cron.daily/logrotate
-sudo touch /etc/cron.daily/logrotate
+echo "Backing up old logrotate configuration..."
+sudo mv /etc/logrotate.conf /var/www/html/emoncms/scripts/logger/logrotate.conf.old
+sudo chown root /var/www/html/emoncms/scripts/logger/logrotate.conf
+echo "Linked to new logrotate configuration..."
+sudo ln -s /var/www/html/emoncms/scripts/logger/logrotate.conf /etc/logrotate.conf
+echo "Backing up old logrotate cron job..."
+sudo mv /etc/cron.daily/logrotate /var/www/html/emoncms/scripts/logger/logrotate.old
+echo "Linked to new logrotate cron job..."
+sudo chmod a+x /var/www/html/emoncms/scripts/logger/logrotate
+sudo ln -s /var/www/html/emoncms/scripts/logger/logrotate /etc/cron.daily/logrotate
+echo ""
+echo "Completed"
+echo ""
 
 echo "setup logrotate state & logfile in /var/log/logrotate"
 sudo mkdir /var/log/logrotate
-chown -R pi:pi /var/log/logrotate
+sudo chown -R root:adm /var/log/logrotate
 
 echo ""
 echo "Completed"
