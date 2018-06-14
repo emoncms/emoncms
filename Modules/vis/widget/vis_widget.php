bd0e2b8ac5dafbbe11719222b6268f1c49222602
Aymeric THIBAUT <thib66@users.noreply.github.com>
2018-06-14 14:12:31 +0200

    Localization vis in dashboard (#846)
    * change domain for translation
    
    * change domain for translation
    
    * Change domain for translation
    
    * Change of domain for translation
    
    * Change of domain for translation
    
    * Change of domain for translation
    
    * Delete messages.mo
    
    * Delete messages.po
    
    * Create vis_messages.po
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete messages.mo
    
    * Delete messages.po
    
    * Add files via upload
    
    * Delete messages.po
    
    * Delete messages.mo
    
    * Add files via upload
    
    * Delete messages.po
    
    * Delete messages.mo
    
    * Add files via upload
    
    * Delete messages.mo
    
    * Delete messages.po
    
    * Add files via upload
    
    * Delete messages.po
    
    * Delete messages.mo
    
    * Add files via upload
    
    * Delete messages.mo
    
    * Delete messages.po
    
    * Add files via upload
    
    * Delete messages.po
    
    * Delete messages.mo
    
    * rename of function due to change domain for i12n
    
    * Change simple quote by double quotes
    
    Due to sentences containing single quote
    
    * change single quotes by double quotes
    
    * Change of function due to change of domain translation

--- Modules/vis/widget/vis_widget.php
+++ Modules/vis/widget/vis_widget.php
@@ -3,6 +3,10 @@
 
     global $mysqli, $session;
 
+    $domain3 = "vis_messages";
+    bindtextdomain($domain3, "Modules/vis/locale");
+    bind_textdomain_codeset($domain3, 'UTF-8');
+
     require "Modules/vis/multigraph_model.php";
     $multigraph = new Multigraph($mysqli);
     $multigraph_list = $multigraph->getlist($session['userid']);
