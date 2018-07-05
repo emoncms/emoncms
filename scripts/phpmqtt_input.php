0009b535f222b9194c41739148a33710de7a5a65
pb66 <pb66@outlook.com>
2018-07-05 17:26:59 +0100

    typo?

--- scripts/phpmqtt_input.php
+++ scripts/phpmqtt_input.php
@@ -102,7 +102,7 @@
         $id (string) – The client ID. If omitted or null, one will be generated at random.
         $cleanSession (boolean) – Set to true to instruct the broker to clean all messages and subscriptions on disconnect. Must be true if the $id parameter is null.
     */ 
-    $mqtt_client = new Mosquitto\Client($mqtt_server['clientid'],($mqtt_server['clientid'] && $mqtt_server['sub_qos']) ? 'false' : 'true';);
+    $mqtt_client = new Mosquitto\Client($mqtt_server['clientid'],($mqtt_server['clientid'] && $mqtt_server['sub_qos']) ? 'false' : 'true' );
     
     $connected = false;
     $last_retry = 0;
