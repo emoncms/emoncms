19b0dc0630a4aa4c112e68dd5cea7744ee6a05ab
Emrys <emrys@openenergymonitor.org>
2018-09-21 00:58:33 +0100

    allowed the config settings to be loaded as an include or directly(json) integrated the new config file into the feed view (v2)

--- units.php
+++ units.php
@@ -2,10 +2,14 @@
 /**
  * load a config file and return units it as json
  */
+namespace emoncms\units;
 
 require __DIR__ . '/vendor/autoload.php';
 use Yosymfony\Toml\Toml;
 // https://github.com/yosymfony/toml
+
+// currently only returns the 'units' part of the config file
+// @todo: this would be better as a generic config reader with a "units" option selected
 
 $filename = 'custom-config.toml';
 try {
@@ -13,8 +17,17 @@ try {
 } catch (Exception $e) {
     $config = array("success"=>false,"message"=>$e->getMessage());
 }
-header('Content-Type: application/json');
-echo json_encode($config['units'],JSON_UNESCAPED_UNICODE);
+$includes = get_included_files();
+// if this script is not called directly (included)
+if(array_search(__FILE__, $includes)>0) {
+    // set the UNIT const
+    if(!empty($config['units'])) define('UNITS', $config['units']);
+} else {
+// if this script is called directly (not included)
+    // return the values as a json object
+    header('Content-Type: application/json');
+    echo json_encode($config['units'], JSON_UNESCAPED_UNICODE);    // this script is being included by another
+}
 
 
 /**
