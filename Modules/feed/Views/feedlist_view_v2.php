c0e7141dec4bd5260f2599e1242b6a1ae1076377
Emrys <emrys@openenergymonitor.org>
2018-09-26 15:01:46 +0100

    made tooltips line up with mouse movements

--- Modules/feed/Views/feedlist_view_v2.php
+++ Modules/feed/Views/feedlist_view_v2.php
@@ -78,17 +78,19 @@ body{padding:0!important}
     max-width:80%;
     position:absolute;
 }
-
+.tooltip {pointer-events: none} /* stops js from triggering mouseleave on underlying element */
 .tooltip h4{margin-bottom:0}
 .tooltip .dl-horizontal{ margin-bottom: .5em;}
 .tooltip .dl-horizontal dt{ width:8.5em }
 .tooltip .dl-horizontal dd{ margin-left:9.3em; text-align: left;min-width:8.5em }
 
+#mouse-position{position:absolute;z-index:999999;width:0em;height:0em;background:red}
 @media (min-width: 768px) {
     .container-fluid { padding: 0px 20px 0px 20px; }
 }
 
 </style>
+<div id="mouse-position"></div>
 <div id="apihelphead" style="float:right; padding-top:10px"><a href="<?php echo $path; ?>feed/api"><?php echo _('Feed API Help'); ?></a></div>
 <div id="localheading"><h3><?php echo _('Feeds'); ?></h3></div>
 
@@ -383,7 +385,7 @@ body{padding:0!important}
           // display nodes and feeds
           var counter = 0
           // remove any tooltips added in previous list
-          $('#table [data-toggle="tooltip"]').tooltip('destroy')
+        //   $('#table [data-toggle="tooltip"]').tooltip('destroy')
           for (var node in nodes) {
               counter ++;
               isCollapsed = !nodes_display[node]
@@ -442,17 +444,20 @@ body{padding:0!important}
           }
           $container = $('#table')
           $container.html(out);
-          // add the tooltips to all the rows
-          $('#table [data-toggle="tooltip"]').tooltip({
-              container: 'body',
-              html: true,
-            //   trigger: 'manual',
-              animation: true
+
+          $tooltips = $('#table [data-toggle="tooltip"]')
+          $tooltips.each(function(){
+              let $this = $(this)
+              if($this.data('tooltip')) $this.tooltip('destory')
           })
+          
+          // add the tooltips to all the rows
+          $tooltips.tooltip(tooltipOptions)
+          .on('show', onTooltipShow)
+          .on('shown',onTooltipShown)
 
           // reset the toggle state for all collapsable elements once data has loaded
           // css class "in" is used to remember the expanded state of the ".collapse" element
-    
           if(typeof $.fn.collapse == 'function'){
             $("#table .collapse").collapse({toggle: false})
             setExpandButtonState($container.find('.collapsed').length == 0)
@@ -461,8 +466,92 @@ body{padding:0!important}
         autowidth($container) // set each column group to the same width
       } // end of for loop
       }); // end of ajax callback
-  }
-  
+  }// end of update() function
+
+var tooltipOptions = {
+    container: '#mouse-position',
+    html: true,
+    delay: { show: 500, hide: 100 },
+    trigger: 'hover',
+    placement: null,
+    animation: false,
+    template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>'
+}
+var tooltipTimeoutIndex = void 0
+// remove previous tooltips if interrupted by new ajax call
+// reset the floating container
+function onTooltipShow(e){
+    let $tooltipContainer = $('#mouse-position')
+    let $tooltip = $tooltipContainer.find('.tooltip')
+    if($tooltip.length > 0) {
+        $tooltip.remove()
+        $tooltipContainer.css({
+            left: mousePos.x,
+            top: mousePos.y
+        })
+    }
+}
+// calculate the position of the tooltip based on mouse and node row position.
+// diplayed to left of mouse if enought space available
+function onTooltipShown(event){ 
+    let $node = event.target.classList.contains('node-feed') ? $(event.target) : $(event.target).parents('.node-feed')
+    if ($node.length==0) return false
+    let tooltipOffset = 10
+    let $tooltipContainer = $('#mouse-position')
+    let $tooltip = $tooltipContainer.find('.tooltip')
+    let mouseX = mousePos.x
+    let mouseY = mousePos.y
+    let tooltipBox = $tooltip[0].getBoundingClientRect()
+    let nodeRowBox = $node[0].getBoundingClientRect()
+    let pointerBox = $tooltipContainer[0].getBoundingClientRect()
+    let distanceFromElementTop = Math.abs(mouseY-nodeRowBox.top)
+    let distanceFromElementBottom = Math.abs(mouseY-nodeRowBox.bottom)
+    let vOffset = nodeRowBox.height/2
+    if(distanceFromElementBottom < distanceFromElementTop){
+        vOffset = -Math.abs(nodeRowBox.height/2)
+    }
+    let left = mouseX+tooltipOffset+tooltipBox.width < $(window).width() ? mouseX+tooltipOffset : $(window).width()-tooltipBox.width-10
+    let top = nodeRowBox.top + nodeRowBox.height/2
+    
+    // only move the tooltip if not already set on the node
+    if(!intersectRect(nodeRowBox,pointerBox)){
+        $tooltipContainer.css({
+            left: left,
+            top: top
+        })
+    }
+    $('#mouse-position').data('tooltip-shown',true)
+}
+
+// store current mouse position in global scope
+document.onmousemove = function(event) {
+    var dot, eventDoc, doc, body, pageX, pageY;
+    event = event || window.event; // IE-ism
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+            (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+            (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+    mousePos = {
+        x: event.pageX,
+        y: event.pageY
+    };
+}
+
+
+function intersectRect(r1, r2) {
+    return !(r2.left > r1.right || 
+        r2.right < r1.left || 
+        r2.top > r1.bottom ||
+        r2.bottom < r1.top);
+}
   // stop checkbox form opening graph view
   $("#table").on("click",".tbody .select",function(e) {
       e.stopPropagation();
@@ -1311,6 +1400,5 @@ function parse_timepicker_time(timestr){
 
     return new Date(date[2],date[1]-1,date[0],time[0],time[1],time[2],0).getTime() / 1000;
 }
-
 
 </script>
