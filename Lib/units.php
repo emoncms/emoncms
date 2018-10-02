d0640b0718b7de61ad05dd6095ecbe215ebda895
Emrys <emrys@openenergymonitor.org>
2018-10-02 11:55:15 +0100

    removed php shorthand arrays

--- Lib/units.php
+++ Lib/units.php
@@ -4,21 +4,21 @@
  */
 namespace emoncms\units;
 
-$config['units'] = [
-    ["short" => "W", "long" => "Watt"],
-    ["short" => "kWh", "long" => "Kilowatt Hour"],
-    ["short" => "Wh", "long" => "Watt-Hour"],
-    ["short" => "V", "long" => "Volt"],
-    ["short" => "VA", "long" => "Volt-Ampere"],
-    ["short" => "A", "long" => "Ampere"],
-    ["short" => "°C", "long" => "Celsius"],
-    ["short" => "K", "long" => "Kelvin"],
-    ["short" => "°F", "long" => "Fahrenheit"],
-    ["short" => "%", "long" => "Percent"],
-    ["short" => "Hz", "long" => "Hertz"],
-    ["short" => "pulses", "long" => "Pulses"],
-    ["short" => "dB", "long" => "Decibel"]
-];
+$config['units'] = array(
+    array("short" => "W", "long" => "Watt"),
+    array("short" => "kWh", "long" => "Kilowatt Hour"),
+    array("short" => "Wh", "long" => "Watt-Hour"),
+    array("short" => "V", "long" => "Volt"),
+    array("short" => "VA", "long" => "Volt-Ampere"),
+    array("short" => "A", "long" => "Ampere"),
+    array("short" => "°C", "long" => "Celsius"),
+    array("short" => "K", "long" => "Kelvin"),
+    array("short" => "°F", "long" => "Fahrenheit"),
+    array("short" => "%", "long" => "Percent"),
+    array("short" => "Hz", "long" => "Hertz"),
+    array("short" => "pulses", "long" => "Pulses"),
+    array("short" => "dB", "long" => "Decibel")
+);
 // list of PHP includes
 $includes = get_included_files();
 // if this script is not called directly (included)
