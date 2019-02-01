b631bf4abd41b2ce57db129256d5b10dee204f66
Emrys <emrys@openenergymonitor.org>
2019-02-01 09:54:17 +0000

    separated locale file loading from feed list view

--- Modules/feed/Views/feedlist_view_v2.php
+++ Modules/feed/Views/feedlist_view_v2.php
@@ -7,26 +7,12 @@
 
 <script src="<?php echo $path; ?>Lib/moment.min.js"></script>
 <script>
-user.lang = "<?php echo $_SESSION['lang']; ?>"
-// rework to fit the momentjs naming scheme for the locale files
-momentjs_locales = {
-    da_DK:'da',
-    nl_BE:'nl-be',
-    nl_NL:'nl',
-    en_GB:'en-gb',
-    et_EE:'et',
-    fr_FR:'fr',
-    de_DE:'de',
-    it_IT:'it',
-    es_ES:'es',
-    cy_GB:'cy'
-}
-// match supported locales with momentjs file names
-user.locale = momentjs_locales.hasOwnProperty(user.lang) ? momentjs_locales[user.lang] : 'en-gb'
-// load the moment js locale file for the user's language
-var script = document.createElement('script');
-script.src = "<?php echo $path; ?>Lib/momentjs-locales/%s.js".replace("%s",user.locale);
-document.head.appendChild(script);
+    var user = {};
+    var path = "<?php echo $path; ?>";
+    user.lang = "<?php echo $_SESSION['lang']; ?>";
+</script>
+<script src="<?php echo $path; ?>Lib/user_locale.js"></script>
+<script>
 
 /**
  * uses moment.js to format to local time 
