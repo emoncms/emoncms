f848e6a416bb67a7ecddadf5e73c80b420edc483
Emrys <emrys@openenergymonitor.org>
2019-02-01 10:53:03 +0000

    added callback _locale_loaded()

--- Lib/user_locale.js
+++ Lib/user_locale.js
@@ -1,4 +1,4 @@
-(function(user){
+(function(user, callback){
     if (!user) return;
 
     // rework to fit the momentjs naming scheme for the locale files
@@ -18,7 +18,11 @@
     user.locale = momentjs_locales.hasOwnProperty(user.lang) ? momentjs_locales[user.lang] : 'en-gb'
     // load the moment js locale file for the user's language
     var script = document.createElement('script');
-    script.src = path + "Lib/momentjs-locales/%s.js".replace("%s",user.locale);
-    document.head.appendChild(script);
+    script.src = path + "Lib/momentjs-locales/%s.js".replace("%s", user.locale);
+    script.id = '__user_loaded_locale';
+    if(typeof callback == 'function') {
+        script.addEventListener("load", callback);
+    }
+    document.getElementsByTagName("script")[0].parentNode.appendChild(script);
 
-})(typeof user !== 'undefined' ? user: null);
+})(typeof user !== 'undefined' ? user: null, typeof _locale_loaded !== 'undefined' ? _locale_loaded: null);
