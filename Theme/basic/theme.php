c49aa9008908b14dff86d74c0ee9f36609f3fce1
Emrys <emrys@openenergymonitor.org>
2019-04-05 17:42:56 +0100

    added calendar icon

--- Theme/basic/theme.php
+++ Theme/basic/theme.php
@@ -232,6 +232,10 @@ if (!in_array($themecolor, ["blue", "sun", "standard"])) {
             <!-- <title>arrow_back</title> -->
             <path d="M26.688 14.688v2.625h-16.25l7.438 7.5-1.875 1.875-10.688-10.688 10.688-10.688 1.875 1.875-7.438 7.5h16.25z"></path>
         </symbol>
+        <symbol id="icon-calendar" viewBox="0 0 32 32">
+            <!-- <title>calendar</title> -->
+            <path d="M10 12h4v4h-4zM16 12h4v4h-4zM22 12h4v4h-4zM4 24h4v4h-4zM10 24h4v4h-4zM16 24h4v4h-4zM10 18h4v4h-4zM16 18h4v4h-4zM22 18h4v4h-4zM4 18h4v4h-4zM26 0v2h-4v-2h-14v2h-4v-2h-4v32h30v-32h-4zM28 30h-26v-22h26v22z"></path>
+        </symbol>
     </defs>
 </svg>
 
