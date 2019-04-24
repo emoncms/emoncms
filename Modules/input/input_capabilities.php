090c195e571f483356602df690c9c78ec7174560
Christian Landvogt <landvogt@isfh.de>
2019-04-24 10:32:25 +0200

    Simple capabilities for input module

---
+++ Modules/input/input_capabilities.php
@@ -0,0 +1,8 @@
+<?php
+
+$capabilities['input'] = [
+	'input_enabled'            => 'Enable module',
+	'input_menuitem'           => 'Show module menu item',
+	'input_write'              => 'writing data to inputs, modifying inputs',
+	'input_read'               => 'read input value',
+];
--- Modules/input/input_menu.php
+++ Modules/input/input_menu.php
@@ -4,4 +4,6 @@
     bindtextdomain($domain, "Modules/input/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
 
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Inputs"), 'icon'=>'icon-arrow-right', 'path'=>"input/view" , 'session'=>"write", 'order' => 10 );
+    if (user_has_capability('input_menuitem')) {
+        $menu_dropdown_config[] = array('name'=> dgettext($domain, "Inputs"), 'icon'=>'icon-arrow-right', 'path'=>"input/view" , 'session'=>"write", 'order' => 10 );
+    }
