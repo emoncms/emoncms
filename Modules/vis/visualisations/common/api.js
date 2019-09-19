0ada50df109cb34a75fb96a31245474b1babd735
Dave <dave_webber38@hotmail.com>
2019-09-19 14:44:07 +0100

    replace eval()
    eval() is marked as "can be harmful" this PR removes the use of eval() with obj()
    I am not 100% certain this is the correct change, but testing shows it to not cause any issues or complaints with the rendering.

--- Modules/vis/visualisations/common/api.js
+++ Modules/vis/visualisations/common/api.js
@@ -1,9 +1,9 @@
 // Get feed list
 function get_feed_list(apikey){
   var list = [];
-  $.ajax({                    
+  $.ajax({
     type: "GET",
-    url: path+"feed/list.json?apikey="+apikey,       
+    url: path+"feed/list.json?apikey="+apikey,
     dataType: 'json',
     async: false,
     success: function(dt) { list = dt; }
@@ -16,10 +16,10 @@ function get_feed_data(feedID,start,end,interval,skipmissing,limitinterval){
   var feedIn = [];
   var query = "id="+feedID+"&start="+start+"&end="+end+"&interval="+interval+"&skipmissing="+skipmissing+"&limitinterval="+limitinterval;
   if (apikey!="") query+= "&apikey="+apikey;
-  $.ajax({                  
-    url: path+'feed/data.json',             
-    data: query,  
-    dataType: 'json',               
+  $.ajax({
+    url: path+'feed/data.json',
+    data: query,
+    dataType: 'json',
     async: false,
     success: function(dt) { feedIn = dt; }
   });
@@ -31,10 +31,10 @@ function get_feed_data_DMY(feedID,start,end,mode){
   var feedIn = [];
   var query = "id="+feedID+"&start="+parseInt(start)+"&end="+parseInt(end)+"&mode="+mode;
   if (apikey!="") query+= "&apikey="+apikey;
-  $.ajax({                  
-    url: path+'feed/data.json',             
-    data: query,  
-    dataType: 'json',               
+  $.ajax({
+    url: path+'feed/data.json',
+    data: query,
+    dataType: 'json',
     async: false,
     success: function(dt) { feedIn = dt; }
   });
@@ -45,12 +45,12 @@ function get_feed_data_DMY(feedID,start,end,mode){
 function get_feed_data_async(callback,context,feedID,start,end,interval,skipmissing,limitinterval){
   var query = "id="+feedID+"&start="+start+"&end="+end+"&interval="+interval+"&skipmissing="+skipmissing+"&limitinterval="+limitinterval;
   if (apikey!="") query+= "&apikey="+apikey;
-  return $.ajax({                  
-    url: path+'feed/data.json',             
-    data: query,  
-    dataType: 'json',   
-    async: true,  
-    success: function(dt) { 
+  return $.ajax({
+    url: path+'feed/data.json',
+    data: query,
+    dataType: 'json',
+    async: true,
+    success: function(dt) {
       if ( typeof callback === "function" ) {
         callback(context,dt);
       }
@@ -62,12 +62,12 @@ function get_feed_data_async(callback,context,feedID,start,end,interval,skipmiss
 function get_feed_data_DMY_async(callback,context,feedID,start,end,mode){
   var query = "id="+feedID+"&start="+start+"&end="+end+"&mode="+mode;
   if (apikey!="") query+= "&apikey="+apikey;
-  return $.ajax({                  
-    url: path+'feed/data.json',             
+  return $.ajax({
+    url: path+'feed/data.json',
     data: query,
-    dataType: 'json',   
-    async: true,  
-    success: function(dt) { 
+    dataType: 'json',
+    async: true,
+    success: function(dt) {
       if ( typeof callback === "function" ) {
         callback(context,dt);
       }
@@ -78,10 +78,10 @@ function get_feed_data_DMY_async(callback,context,feedID,start,end,mode){
 // Get histogram data
 function get_histogram_data(feedID,start,end){
   var feedIn = [];
-  $.ajax({                  
-    url: path+'feed/histogram.json',             
-    data: "id="+feedID+"&start="+start+"&end="+end+"&apikey="+apikey,  
-    dataType: 'json',               
+  $.ajax({
+    url: path+'feed/histogram.json',
+    data: "id="+feedID+"&start="+start+"&end="+end+"&apikey="+apikey,
+    dataType: 'json',
     async: false,
     success: function(dt) { feedIn = dt; }
   });
@@ -91,12 +91,12 @@ function get_histogram_data(feedID,start,end){
 // Get kwh per day at power range
 function get_kwhatpower(feedid,rmin,rmax){
   var feedIn = [];
-  $.ajax({                    
-    url: path+'feed/kwhatpower.json',             
+  $.ajax({
+    url: path+'feed/kwhatpower.json',
     data: "id="+feedid+"&min="+rmin+"&max="+rmax+"&apikey="+apikey,
     dataType: 'json',
-    async: false,            
-    success: function(dt) { feedIn = dt; } 
+    async: false,
+    success: function(dt) { feedIn = dt; }
   });
   return feedIn;
 }
@@ -106,12 +106,12 @@ function get_multigraph(apikey){
   var apikey = "";
   if (apikey!="") apikey= "apikey="+apikey;
   var feedlist = [];
-  $.ajax({                    
+  $.ajax({
     type: "GET",
     url: path+"vis/multigraphget.json",
-    data: apikey, 
-    async: false,  
-    dataType: 'json',   
+    data: apikey,
+    async: false,
+    dataType: 'json',
     success: function(dt){feedlist = dt;}
   });
   return feedlist;
@@ -119,9 +119,9 @@ function get_multigraph(apikey){
 
 // Get feed data
 function save_multigraph(write_apikey,feedlist){
-  var feedlist_save = eval(JSON.stringify(feedlist));
+  var feedlist_save = obj(JSON.stringify(feedlist));
   for(var i in feedlist_save) { feedlist_save[i].plot.data = null; }
-  $.ajax({                    
+  $.ajax({
     type: "POST",
     url: path+"vis/multigraphsave.json",
     data: "data="+JSON.stringify(feedlist_save)+"&apikey="+write_apikey,
