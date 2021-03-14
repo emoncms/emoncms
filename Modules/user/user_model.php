fd82d0cb4c35a533ac7cdb1dd7df43d7c8b2009c
TrystanLea <trystan.lea@gmail.com>
2021-03-14 14:18:27 +0000

    use default UTC timezone if timezone is not valid

--- Modules/user/user_model.php
+++ Modules/user/user_model.php
@@ -234,7 +234,10 @@ class User
         if (strlen($username) < 3 || strlen($username) > 30) return array('success'=>false, 'message'=>_("Username length error"));
         if (strlen($password) < 4 || strlen($password) > 250) return array('success'=>false, 'message'=>_("Password length error"));
         
-        if (!$this->timezone_valid($timezone)) return array('success'=>false, 'message'=>_("Timezone is not valid"));
+        if (!$this->timezone_valid($timezone)) {
+            // use default UTC timezone if timezone is not valid
+            $timezone = "UTC";
+        }
 
         // If we got here the username, password and email should all be valid
 
