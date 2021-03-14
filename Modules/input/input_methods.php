6319ea8ea18ed2c53d75a175516af0f05a4c44df
TrystanLea <trystan.lea@gmail.com>
2021-03-14 14:29:28 +0000

    remove time key so as not to create a related input

--- Modules/input/input_methods.php
+++ Modules/input/input_methods.php
@@ -111,7 +111,8 @@ class InputMethods
                     $log->info("Time from parameter used");
                 } elseif (array_key_exists('time',$jsondataLC)){
                     $inputtime = $jsondataLC['time'];
-
+                    // remove time key so as not to create a related input
+                    unset($jsondata['time']);
                     // validate time
                     if (is_numeric($inputtime)){
                         $log->info("Valid time in seconds used ".$inputtime);
