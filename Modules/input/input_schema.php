47ffa382c8065ad19f27972d2f7e5e75cef49955
brucemiranda <brucemiranda@gmail.com>
2021-07-06 23:22:39 +0100

    Update input_schema.php

--- Modules/input/input_schema.php
+++ Modules/input/input_schema.php
@@ -1,12 +1,12 @@
 <?php
 
 $schema['input'] = array(
-    'id' => array('type' => 'int(11)', 'Null'=>false, 'Key'=>'PRI', 'Extra'=>'auto_increment'),
-    'userid' => array('type' => 'int(11)'),
+    'id' => array('type' => 'int', 'Null'=>false, 'Key'=>'PRI', 'Extra'=>'auto_increment'),
+    'userid' => array('type' => 'int'),
     'nodeid' => array('type' => 'text'),
     'name' => array('type' => 'text'),
     'description' => array('type' => 'text'),
     'processList' => array('type' => 'text'),
-    'time' => array('type' => 'int(10)'),
+    'time' => array('type' => 'int'),
     'value' => array('type' => 'float')
 );
