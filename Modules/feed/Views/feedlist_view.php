2d744a7579c6ffaf0352386973770608f8b11d03
TrystanLea <trystan.lea@gmail.com>
2021-12-09 16:46:51 +0000

    fix settings ini hidden egine array parse issue

--- Modules/feed/Views/feedlist_view.php
+++ Modules/feed/Views/feedlist_view.php
@@ -397,7 +397,7 @@ var selected_feeds = {};
 var local_cache_key = 'feed_nodes_display';
 var nodes_display = {};
 var feed_engines = ['MYSQL','TIMESTORE','PHPTIMESERIES','GRAPHITE','PHPTIMESTORE','PHPFINA','PHPFIWA (No longer supported)','VIRTUAL','MEMORY','REDISBUFFER','CASSANDRA'];
-var engines_hidden = JSON.parse(<?php echo json_encode($settings["feed"]['engines_hidden']); ?>);
+var engines_hidden = <?php echo json_encode($settings["feed"]['engines_hidden']); ?>;
 
 var available_intervals = <?php echo json_encode(Engine::available_intervals()); ?>;
 var tmp = []; for (var z in available_intervals) tmp.push(available_intervals[z]['interval']); available_intervals = tmp;
