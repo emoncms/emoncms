5d8d3331d2209f0f74a69975e70cd3121b8365a9
TrystanLea <trystan.lea@gmail.com>
2021-12-12 21:08:52 +0000

    redisbuffer implementation of new get_data_combined format

--- Modules/feed/engine/RedisBuffer.php
+++ Modules/feed/engine/RedisBuffer.php
@@ -125,30 +125,105 @@ class RedisBuffer implements engine_methods
      * @param integer $skipmissing not implemented
      * @param integer $limitinterval not implemented
     */
-    public function get_data_combined($feedid,$start,$end,$interval,$average=0,$timezone="UTC",$timeformat="unix",$csv=false,$skipmissing=0,$limitinterval=1)
-    {
-        $feedid = intval($feedid);
-        $start = intval($start);
-        $end = intval($end);
-        $data = array();
-
-        $len = $this->redis->zCount("feed:$feedid:buffer",$start,$end);
-        // process if there is data on buffer for the range
-        if ($len > 0) {
-            $this->log->info("get_data_combined() feed=$feedid len=$len start=$start end=$end");
-            $range = 50000; // step range number of points to extract on each iteration 50k-100k is ok
-            for ($i=0; $i<=$len; $i = $i + $range)
-            {
-                $buf_item = $this->redis->zRangeByScore("feed:$feedid:buffer", $start, $end, array('withscores' => true, 'limit' => array($i, $range)));
-                foreach($buf_item as $rawvalue => $time) {
-                    $f = explode("|",$rawvalue);
-                    $value = $f[1];
-                    $data[$time] = array($time,(float)$value);
-                }
-            }
-            $data = array_values($data); // re-index array
+    public function get_data_combined($id,$start,$end,$interval,$average=0,$timezone="UTC",$timeformat="unix",$csv=false,$skipmissing=0,$limitinterval=1)
+    {   
+        $id = (int) $id;
+        $skipmissing = (int) $skipmissing;
+        $limitinterval = (int) $limitinterval;
+        
+        if ($end<=$start) return array('success'=>false, 'message'=>"request end time before start time");
+        
+        if ($timezone===0) $timezone = "UTC";
+       
+        if ($csv) {
+            require_once "Modules/feed/engine/shared_helper.php";
+            $helperclass = new SharedHelper($settings['feed']);
+            $helperclass->set_time_format($timezone,$timeformat);
         }
-        return $data;
+
+        // The first section here deals with the timezone aligned interval codes
+        // the start time is modified to align to the nearest day, week, month or year
+        // later the while loop is advanced by the value in the $modify string
+        // all using php DateTime aligned to user/feed timezone
+        if (in_array($interval,array("weekly","daily","monthly","annual"))) {
+            $fixed_interval = false;
+            // align to day, month, year
+            $date = new DateTime();
+            $date->setTimezone(new DateTimeZone($timezone));
+            $date->setTimestamp($start);
+            $date->modify("midnight");
+            $modify = "+1 day";
+            if ($interval=="weekly") {
+                $date->modify("this monday");
+                $modify = "+1 week";
+            } else if ($interval=="monthly") {
+                $date->modify("first day of this month");
+                $modify = "+1 month";
+            } else if ($interval=="annual") {
+                $date->modify("first day of january this year");
+                $modify = "+1 year";
+            }
+            // Set time to start 
+            $time = $date->getTimestamp();
+        } else {
+            // If interval codes are not specified then we advanced by a fixed numeric interval 
+            $fixed_interval = true;
+            // Interval must be integer
+            $interval = (int) $interval;
+            // Interval should not be less than 1 second
+            if ($interval<1) $interval = 1;
+            // May want to limit to data interval here if feed engine has a fixed interval type
+            // Set time to start  
+            $time = $start;
+        }
+
+        if ($csv) {
+            $helperclass->csv_header($id);
+        } else {
+            $data = array();
+        }
+
+        while($time<=$end)
+        {
+            // Start time of interval/division
+            $div_start = $time;
+            
+            // calculate start of next interval 
+            if ($fixed_interval) {
+                $div_end = $time + $interval;
+            } else {
+                $date->modify($modify);
+                $div_end = $date->getTimestamp();
+            }
+            
+            // Read in value here from data file, database, timeseries interface
+            // If average = 0, find nearest value that is >= div_start && < div_end
+            // If average = 1, find average of values that are >= div_start && < div_end
+            $value = null;
+            
+            $dp = $this->redis->zRangeByScore("feed:$id:buffer",$time,($div_end-1), array('limit' => array(0,1)));
+            if (count($dp)>0) {
+                $f = explode("|",$dp[0]);    
+                $value = (float) $f[1];
+            }
+            
+            // Write as csv or array
+            if ($csv) { 
+                $helperclass->csv_write($div_start,$value);
+            } else {
+                $data[] = array($div_start*1000,$value);
+            }
+
+            // Advance position 
+            $time = $div_end;        
+        }
+        
+        if ($csv) {
+            $helperclass->csv_close();
+            exit;
+        } else {
+            return $data;
+        }
     }
 
     public function export($feedid,$start)
