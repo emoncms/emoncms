emoncms/emoncms

commit cc8d5260f3df106e1e6cc7d50ab14fd1f8e907b2

harden getServiceStatus and setService

Modules/admin/admin_model.php
@@ -173,6 +173,11 @@ class Admin {
      * @return array | true == running | false == stopped | empty == not installed
      */
     public function getServiceStatus($name) {
+        // Validate service name
+        if (!in_array($name, $this->get_services_list())) {
+            return array();
+        }
+
         if (!$exec = $this->exec_array('systemctl show '.$name.' | grep State')) {
             return array();
         }
@@ -194,11 +199,17 @@ class Admin {
     }
 
     public function setService($name, $action) {
+        // $action = start | stop | restart | enable | disable
+        if (!in_array($action, array('start','stop','restart','enable','disable'))) {
+            return array('success'=>false, 'message'=>"Invalid action '$action'");
+        }
+
         $script = __DIR__ . "/../../scripts/service-action.sh";
         return $this->runService($script, "$name $action");
     }
 
-    public function runService($script, $attributes) {
+    private function runService($script, $attributes) {
+        
         if (!file_exists($script)) {
             $this->log->error("runService() Script not found '$script' attributes=$attributes");
             return array('success'=>false, 'message'=>"File not found '$script' attributes=$attributes");

commit ef0299fa82291e4eacd99cd7e70e0dcc6d351d60

admin_model.php: Make runService public again
Fixes issue caused in cc8d5260f3df106e1e6cc7d50ab14fd1f8e907b2

Reported in https://community.openenergymonitor.org/t/cant-update-emoncms-via-gui/26898

Modules/admin/admin_model.php
@@ -210,8 +210,8 @@ class Admin {
         return $this->runService($script, "$name $action");
     }
 
-    private function runService($script, $attributes) {
-        
+    public function runService($script, $attributes) {
+
         if (!file_exists($script)) {
             $this->log->error("runService() Script not found '$script' attributes=$attributes");
             return array('success'=>false, 'message'=>"File not found '$script' attributes=$attributes");