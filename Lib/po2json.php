f40d244c53f6c825431347df1b31341a24935d19
Trystan Lea <trystan.lea@gmail.com>
2025-07-06 14:35:19 +0100

    improved po2json conversion

--- Lib/po2json.php
+++ Lib/po2json.php
@@ -2,30 +2,64 @@
 
 function po2json($input, $output) 
 {
-    $lines = file($input);
+    $lines = file($input, FILE_IGNORE_NEW_LINES);
     $result = array();
-    $msgid = null;
-    $msgstr = null;
+    $msgid = '';
+    $msgstr = '';
     $state = null;
 
     foreach ($lines as $line) {
         $line = trim($line);
-        if (strpos($line, 'msgid "') === 0) {
-            $msgid = stripcslashes(substr($line, 7, -1));
-            $state = 'msgid';
-        } elseif (strpos($line, 'msgstr "') === 0) {
-            $msgstr = stripcslashes(substr($line, 8, -1));
-            $state = 'msgstr';
-            if ($msgid !== null && $msgid !== '' && $msgstr !== null && $msgstr !== '') {
+        
+        // Skip empty lines and comments
+        if (empty($line) || $line[0] === '#') {
+            continue;
+        }
+        
+        if (strpos($line, 'msgid ') === 0) {
+            // Save previous entry if complete
+            if ($state === 'msgstr' && $msgid !== '' && $msgstr !== '') {
                 if (isset($result[$msgid])) {
                     die ("Error: Duplicate msgid found: $msgid\n");
                 }
                 $result[$msgid] = $msgstr;
-                $msgid = null;
-                $msgstr = null;
+            }
+            
+            // Start new msgid
+            if (preg_match('/^msgid\s+"(.*)"$/', $line, $matches)) {
+                $msgid = stripcslashes($matches[1]);
+            } else {
+                $msgid = '';
+            }
+            $msgstr = '';
+            $state = 'msgid';
+            
+        } elseif (strpos($line, 'msgstr ') === 0) {
+            // Start msgstr
+            if (preg_match('/^msgstr\s+"(.*)"$/', $line, $matches)) {
+                $msgstr = stripcslashes($matches[1]);
+            } else {
+                $msgstr = '';
+            }
+            $state = 'msgstr';
+            
+        } elseif (preg_match('/^"(.*)"$/', $line, $matches)) {
+            // Continuation line
+            $content = stripcslashes($matches[1]);
+            if ($state === 'msgid') {
+                $msgid .= $content;
+            } elseif ($state === 'msgstr') {
+                $msgstr .= $content;
             }
         }
-        // Ignore comments and other lines
+    }
+
+    // Save the last entry
+    if ($state === 'msgstr' && $msgid !== '' && $msgstr !== '') {
+        if (isset($result[$msgid])) {
+            die ("Error: Duplicate msgid found: $msgid\n");
+        }
+        $result[$msgid] = $msgstr;
     }
 
     // convert empty array to associative array
