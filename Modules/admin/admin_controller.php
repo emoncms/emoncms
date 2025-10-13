emoncms/emoncms

commit 27cc63e439175dd36d2a74b31ba51dad4684ac3f

improvements from emoncms.org

Modules/admin/admin_controller.php
@@ -49,7 +49,7 @@ function admin_controller()
     // --------------------------------------------------------------------------------------------
     // If not an admin session show notice
     // --------------------------------------------------------------------------------------------
-    if (!$session['admin']) {
+    if (!isset($session['admin']) || !$session['admin']) {
         $route->format = 'html';
         // user not admin level display login
         $log->error(sprintf('%s|%s',tr('Not Admin'), implode('/',array_filter(array($route->controller,$route->action,$route->subaction)))));
@@ -494,6 +494,11 @@ function admin_controller()
     // ----------------------------------------------------------------------------------------
     if ($route->action == 'setuser') {
         $_SESSION['userid'] = intval(get('id'));
+        
+        $u = $user->get($_SESSION['userid']);
+        $_SESSION['username'] = $u->username;
+        $_SESSION['gravatar'] = $u->gravatar;
+        
         header("Location: ../user/view");
         // stop any other code from running once http header sent
         exit();

Modules/input/Views/input_api.php
@@ -1,4 +1,7 @@
-<?php global $path, $session, $user; ?>
+<?php 
+defined('EMONCMS_EXEC') or die('Restricted access');
+global $path, $session, $user; 
+?>
 
 <style>
 td:nth-of-type(1) { width:35%;}

Modules/input/Views/input_dialog.php
@@ -1,4 +1,5 @@
 <?php
+    defined('EMONCMS_EXEC') or die('Restricted access');
     global $path;
 ?>
 <style>

Modules/input/Views/input_view.php
@@ -1,5 +1,6 @@
-<?php $v = 26; ?>
-
+<?php $v=26; 
+defined('EMONCMS_EXEC') or die('Restricted access');
+?>
 <!-- Load dependencies -->
 <?php if ($device_module) { ?>
     <script src="<?php echo $path; ?>Modules/device/Views/device.js?v=27"></script>

Modules/input/input_methods.php
@@ -227,14 +227,19 @@ class InputMethods
         if ($param->exists('cb')) {
             // data is compressed binary format
             $data = file_get_contents('php://input');
-            $data = gzuncompress($data);
+            $data = @gzuncompress($data);
         } elseif ($param->exists('c')) {
             // data is compressed hex format
-            $data = gzuncompress(hex2bin($data));
+            $bindata = hex2bin($data);
+            if (!$bindata) {
+                return "Format error, compressed hex not valid";
+            }
+            $data = @gzuncompress($bindata);
         }
-
+        if ($data==null) return "Format error, json string supplied is not valid";
         $data = json_decode($data);
-
+        if ($data==null) return "Format error, json string supplied is not valid";
+        
         $len = count($data);
 
         if ($len==0) return "Format error, json string supplied is not valid";

Modules/input/input_model.php
@@ -144,6 +144,7 @@ class Input
         $fields = json_decode(stripslashes($fields));
 
         $success = false;
+        $fields_out = array();
 
         if (isset($fields->name)) {
             if (preg_replace('/[^\p{N}\p{L}_\s\-]/u','',$fields->name)!=$fields->name) return array('success'=>false, 'message'=>'invalid characters in input name');
@@ -153,6 +154,7 @@ class Input
             $stmt->close();
 
             if ($this->redis) $this->redis->hset("input:$id",'name',$fields->name);
+            $fields_out['name'] = $fields->name;
         }
 
         if (isset($fields->description)) {
@@ -163,10 +165,16 @@ class Input
             $stmt->close();
 
             if ($this->redis) $this->redis->hset("input:$id",'description',$fields->description);
+            $fields_out['description'] = $fields->description;
         }
 
         if ($success){
-            return array('success'=>true, 'message'=>'Field updated');
+            return array(
+                'success'=>true, 
+                'message'=>'Field updated',
+                'inputid'=>$id,
+                'fields'=>$fields_out
+            );
         } else {
             return array('success'=>false, 'message'=>'Field could not be updated');
         }
@@ -205,7 +213,14 @@ class Input
 
             if ($this->redis) $this->redis->hset("input:$id",'description',$description);
         }
-        return array('success'=>true, 'message'=>'input descriptions updated');
+
+        return array(
+            'success'=>true, 
+            'message'=>'input descriptions updated',
+            'userid'=>$userid,
+            'nodeid'=>$nodeid,
+            'names'=>$names
+        );
     }
 
 
@@ -482,9 +497,16 @@ class Input
 
         if ($this->redis) {
             $this->redis->del("input:$inputid");
+            $this->redis->del("input:lastvalue:$inputid");
             $this->redis->srem("user:inputs:$userid",$inputid);
         }
-        return "input deleted";
+
+        return array(
+            'success'=>true, 
+            'message'=>'Input deleted',
+            'userid'=>$userid,
+            'inputid'=>$inputid
+        );
     }
 
     // userid and inputids are checked in belongs_to_user and delete
@@ -492,7 +514,12 @@ class Input
         foreach ($inputids as $inputid) {
             if ($this->belongs_to_user($userid, $inputid)) $this->delete($userid, $inputid);
         }
-        return "inputs deleted";
+        return array(
+            'success'=>true, 
+            'message'=>'Inputs deleted',
+            'userid'=>$userid,
+            'inputids'=>$inputids
+        );
     }
 
     public function clean($userid)
@@ -505,12 +532,7 @@ class Input
             $inputid = $row['id'];
             if ($row['processList']==NULL || $row['processList']=='')
             {
-                $result = $this->mysqli->query("DELETE FROM input WHERE userid = '$userid' AND id = '$inputid'");
-
-                if ($this->redis) {
-                    $this->redis->del("input:$inputid");
-                    $this->redis->srem("user:inputs:$userid",$inputid);
-                }
+                $this->delete($userid, $inputid);
                 $n++;
             }
         }

commit 575ea24959a9db340075bb6ac858ffb9bc9e6d7a

admin_controller: Use global $user
Fixes https://github.com/emoncms/emoncms/issues/1948

Caused by 27cc63e439175dd36d2a74b31ba51dad4684ac3f

Modules/admin/admin_controller.php
@@ -14,7 +14,7 @@ defined('EMONCMS_EXEC') or die('Restricted access');
 
 function admin_controller()
 {
-    global $settings, $mysqli, $session, $route, $redis, $path, $log;
+    global $settings, $mysqli, $session, $route, $redis, $path, $log, $user;
 
     if (!$session['write']) {
         return array('success'=>false, 'content'=>'', 'reauth'=>true, 'message'=>"Admin re-authentication required");