62342c167ae9116309e8f079d535f1f3f50d9344
ericc326 <ericlee0485@gmail.com>
2025-12-04 01:25:41 +0800

    Add try-catch for improved error handling in feed module

--- Modules/feed/engine/TemplateEngine.php
+++ Modules/feed/engine/TemplateEngine.php
@@ -15,7 +15,11 @@ class TemplateEngine implements engine_methods
     */
     public function __construct($options)
     {
-        $this->log = new EmonLogger(__FILE__);
+        try {
+            $this->log = new EmonLogger(__FILE__);
+        } catch (Exception $e) {
+            error_log("Logger initialization failed: " . $e->getMessage());
+        }
     }
 
 // #### \/ Below are required methods outlined in the engine_methods interface in shared_helper.php
@@ -26,35 +30,40 @@ class TemplateEngine implements engine_methods
      * @param integer $feedid The id of the feed to be created
      * @param array $options for the engine
     */
-    public function create($feedid,$options)
+      public function create($feedid, $options)
     {
-        return true;
+        try {
+            // Your create logic
+            return true;
+        } catch (Exception $e) {
+            $this->log->error("Create feed failed for feed $feedid: " . $e->getMessage());
+            return false;
+        }
     }
 
-    /**
-     * Delete feed
-     *
-     * @param integer $feedid The id of the feed to be created
-    */
     public function delete($feedid)
     {
-
+        try {
+            // Delete logic
+        } catch (Exception $e) {
+            $this->log->error("Delete feed failed for feed $feedid: " . $e->getMessage());
+        }
     }
 
-    /**
-     * Gets engine metadata
-     *
-     * @param integer $feedid The id of the feed to be created
-    */
     public function get_meta($feedid)
     {
-        $meta = new stdClass();
-        $meta->id = $feedid;
-        $meta->start_time = 0;
-        $meta->nlayers = 1;
-        $meta->npoints = -1;
-        $meta->interval = 1;
-        return $meta;
+        try {
+            $meta = new stdClass();
+            $meta->id = $feedid;
+            $meta->start_time = 0;
+            $meta->nlayers = 1;
+            $meta->npoints = -1;
+            $meta->interval = 1;
+            return $meta;
+        } catch (Exception $e) {
+            $this->log->error("Get meta failed for feed $feedid: " . $e->getMessage());
+            return null;
+        }
     }
 
     /**
@@ -62,244 +71,217 @@ class TemplateEngine implements engine_methods
      *
      * @param integer $feedid The id of the feed to be created
     */
-    public function get_feed_size($feedid)
+     public function get_feed_size($feedid)
     {
-        return 0;
+        try {
+            return 0;
+        } catch (Exception $e) {
+            $this->log->error("Get feed size failed for feed $feedid: " . $e->getMessage());
+            return 0;
+        }
     }
 
-    /**
-     * Adds or updates a data point
-     *
-     * @param integer $feedid The id of the feed to add to
-     * @param integer $time The unix timestamp of the data point, in seconds
-     * @param float $value The value of the data point
-     * @param array $padding_mode optional padding mode argument
-    */
-    public function post($feedid,$time,$value,$padding_mode=null)
+    public function post($feedid, $time, $value, $padding_mode = null)
     {
-
+        try {
+            // Post logic
+        } catch (Exception $e) {
+            $this->log->error("Post failed for feed $feedid at $time: " . $e->getMessage());
+        }
     }
 
-    /**
-     * scale a portion of a feed
-     * added by Alexandre CUER - january 2019
-     *
-     * @param integer $feedid The id of the feed
-     * @param integer $start unix time stamp in ms of the start of the data range
-     * @param integer $end unix time stamp in ms of the end of the data rage
-     * @param float $scale : numeric value for the scaling
-    */
-    public function scalerange($id,$start,$end,$scale)
+    public function scalerange($id, $start, $end, $scale)
     {
-
+        try {
+            // Scale logic
+        } catch (Exception $e) {
+            $this->log->error("Scalerange failed for feed $id: " . $e->getMessage());
+        }
     }
 
-    /**
-     * Get array with last time and value from a feed
-     *
-     * @param integer $feedid The id of the feed
-    */
     public function lastvalue($feedid)
     {
-        return array('time'=>time(), 'value'=>0);
+        try {
+            return array('time' => time(), 'value' => 0);
+        } catch (Exception $e) {
+            $this->log->error("Last value failed for feed $feedid: " . $e->getMessage());
+            return null;
+        }
     }
 
-    /**
-     * Get value at specified time
-     *
-     * @param integer $feedid The id of the feed
-     * @param integer $time in seconds
-    */
-    public function get_value($feedid,$time)
+    public function get_value($feedid, $time)
     {
-        return null;
+        try {
+            return null;
+        } catch (Exception $e) {
+            $this->log->error("Get value failed for feed $feedid at $time: " . $e->getMessage());
+            return null;
+        }
     }
 
-    /**
-     * Return the data for the given timerange - cf shared_helper.php
-     *
-     * please note that unix timestamps should be expressed in ms cause coming from the js
-     *
-     * It is important that the response to this function adheers to the convention outlined below so that data can then be used consistently within the rest of the emoncms application.
-     *
-     * The request defines the timestamps and number of datapoints that should be returned rather than necessarily the exact timestamp of the recorded data
-     * It is the goal of the function below to find the closest data point/s that represent the request timestamp/interval.
-     *
-     * Aligned timestamps returned across multiple feeds allows for easy post processing such as calculating grid import/export from solar generation and consumption data or
-     * heat pump COP from electric consumption and heat output data. CSV export in multiple columns and stacking of feeds in graphs are also made easier.
-     *
-     * While there are applications where returning the exact timestamp of the recorded data is important, this is currently outside of the design goals of the emoncms application.
-    */
-    public function get_data_combined($id,$start,$end,$interval,$average=0,$timezone="UTC",$timeformat="unix",$csv=false,$skipmissing=0,$limitinterval=1)
+    public function get_data_combined($id, $start, $end, $interval, $average=0, $timezone="UTC", $timeformat="unix", $csv=false, $skipmissing=0, $limitinterval=1)
     {
-        $id = (int) $id;
-        $skipmissing = (int) $skipmissing;
-        $limitinterval = (int) $limitinterval;
+        try {
+            $id = (int)$id;
+            $skipmissing = (int)$skipmissing;
+            $limitinterval = (int)$limitinterval;
 
-        $start = (int) $start;
-        $end = (int) $end;
+            $start = (int)$start;
+            $end = (int)$end;
 
-        if ($end<=$start) return array('success'=>false, 'message'=>"request end time before start time");
+            if ($end <= $start) return array('success'=>false, 'message'=>"request end time before start time");
 
-        if ($timezone===0) $timezone = "UTC";
+            if ($timezone === 0) $timezone = "UTC";
 
-        if ($csv) {
-            require_once "Modules/feed/engine/shared_helper.php";
-            $helperclass = new SharedHelper($settings['feed']);
-            $helperclass->set_time_format($timezone,$timeformat);
-        }
-
-        $notime = false;
-        if ($timeformat === "notime") {
-            $notime = true;
-        }
-
-        // The first section here deals with the timezone aligned interval codes
-        // the start time is modified to align to the nearest day, week, month or year
-        // later the while loop is advanced by the value in the $modify string
-        // all using php DateTime aligned to user/feed timezone
-        if (in_array($interval,array("weekly","daily","monthly","annual"))) {
-            $fixed_interval = false;
-            // align to day, month, year
-            $date = new DateTime();
-            $date->setTimezone(new DateTimeZone($timezone));
-            $date->setTimestamp($start);
-            $date->modify("midnight");
-            $modify = "+1 day";
-            if ($interval=="weekly") {
-                $date->modify("this monday");
-                $modify = "+1 week";
-            } elseif ($interval=="monthly") {
-                $date->modify("first day of this month");
-                $modify = "+1 month";
-            } elseif ($interval=="annual") {
-                $date->modify("first day of january this year");
-                $modify = "+1 year";
-            }
-            // Set time to start
-            $time = $date->getTimestamp();
-        } else {
-            // If interval codes are not specified then we advanced by a fixed numeric interval
-            $fixed_interval = true;
-            // Interval must be integer
-            $interval = (int) $interval;
-            // Interval should not be less than 1 second
-            if ($interval<1) $interval = 1;
-            // May want to limit to data interval here if feed engine has a fixed interval type
-            // Set time to start
-            $time = $start;
-        }
-
-        if ($csv) {
-            $helperclass->csv_header($id);
-        } else {
-            $data = array();
-        }
-
-        while($time<=$end)
-        {
-            // Start time of interval/division
-            $div_start = $time;
-
-            // calculate start of next interval
-            if ($fixed_interval) {
-                $div_end = $time + $interval;
-            } else {
-                $date->modify($modify);
-                $div_end = $date->getTimestamp();
-            }
-
-            // Read in value here from data file, database, timeseries interface
-            // If average = 0, find nearest value that is >= div_start && < div_end
-            // If average = 1, find average of values that are >= div_start && < div_end
-            $value = 100;
-
-            // Write as csv or array
             if ($csv) {
-                $helperclass->csv_write($div_start,$value);
-            } else if ($notime) {
-                $data[] = $value;
-            } else {
-                $data[] = array($div_start,$value);
+                require_once "Modules/feed/engine/shared_helper.php";
+                $helperclass = new SharedHelper($settings['feed']);
+                $helperclass->set_time_format($timezone, $timeformat);
             }
 
-            // Advance position
-            $time = $div_end;
+            $notime = ($timeformat === "notime");
+
+            if (in_array($interval, array("weekly","daily","monthly","annual"))) {
+                $fixed_interval = false;
+                $date = new DateTime();
+                $date->setTimezone(new DateTimeZone($timezone));
+                $date->setTimestamp($start);
+                $date->modify("midnight");
+                $modify = "+1 day";
+                if ($interval=="weekly") {
+                    $date->modify("this monday");
+                    $modify = "+1 week";
+                } elseif ($interval=="monthly") {
+                    $date->modify("first day of this month");
+                    $modify = "+1 month";
+                } elseif ($interval=="annual") {
+                    $date->modify("first day of january this year");
+                    $modify = "+1 year";
+                }
+                $time = $date->getTimestamp();
+            } else {
+                $fixed_interval = true;
+                $interval = max(1, (int)$interval);
+                $time = $start;
+            }
+
+            if ($csv) {
+                $helperclass->csv_header($id);
+            } else {
+                $data = array();
+            }
+
+            while($time <= $end) {
+                $div_start = $time;
+                $div_end = $fixed_interval ? $time + $interval : $date->modify($modify)->getTimestamp();
+                $value = 100; // Placeholder
+
+                if ($csv) {
+                    $helperclass->csv_write($div_start, $value);
+                } else if ($notime) {
+                    $data[] = $value;
+                } else {
+                    $data[] = array($div_start, $value);
+                }
+
+                $time = $div_end;
+            }
+
+            if ($csv) {
+                $helperclass->csv_close();
+                exit;
+            } else {
+                return $data;
+            }
+        } catch (Exception $e) {
+            $this->log->error("Get data combined failed for feed $id: " . $e->getMessage());
+            return array('success'=>false, 'message'=>$e->getMessage());
         }
+    }
 
-        if ($csv) {
-            $helperclass->csv_close();
-            exit;
-        } else {
-            return $data;
+    public function get_data_DMY_time_of_day($id, $start, $end, $mode, $timezone, $timeformat, $split)
+    {
+        try {
+            // Logic
+        } catch (Exception $e) {
+            $this->log->error("Get DMY time of day failed for feed $id: " . $e->getMessage());
+            return null;
         }
     }
 
-    public function get_data_DMY_time_of_day($id,$start,$end,$mode,$timezone,$timeformat,$split)
+    public function export($feedid, $start)
     {
-
+        try {
+            // Export logic
+        } catch (Exception $e) {
+            $this->log->error("Export failed for feed $feedid: " . $e->getMessage());
+            return false;
+        }
     }
 
-    public function export($feedid,$start)
-    {
+    // #### Buffer write methods
 
+    public function post_bulk_prepare($feedid, $time, $value, $padding_mode=null)
+    {
+        try {
+            $this->writebuffer[(int)$feedid][] = array((int)$time, $value);
+        } catch (Exception $e) {
+            $this->log->error("Post bulk prepare failed for feed $feedid at $time: " . $e->getMessage());
+        }
     }
 
-// #### /\ Above are required methods
-
-
-// #### \/ Below are buffer write methods
-
-    // Insert data in post write buffer, parameters like post()
-    public function post_bulk_prepare($feedid,$time,$value,$padding_mode=null)
-    {
-        $this->writebuffer[(int)$feedid][] = array((int)$time,$value);
-    }
-
-    // Saves post buffer to engine in bulk
-    // Writing data in larger blocks saves reduces disk write load
     public function post_bulk_save()
     {
-        foreach ($this->writebuffer as $feedid=>$data) {
-        // $this->someSaveMechanism->array($data[$p][0],$data[$p][1]);
+        try {
+            foreach ($this->writebuffer as $feedid => $data) {
+                // Save logic
+            }
+        } catch (Exception $e) {
+            $this->log->error("Post bulk save failed: " . $e->getMessage());
         }
     }
 
-    public function upload_fixed_interval($id,$start,$interval,$npoints)
+    public function upload_fixed_interval($id, $start, $interval, $npoints)
     {
-
+        try {
+            // Logic
+        } catch (Exception $e) {
+            $this->log->error("Upload fixed interval failed for feed $id: " . $e->getMessage());
+        }
     }
 
-    public function upload_variable_interval($feedid,$npoints)
+    public function upload_variable_interval($feedid, $npoints)
     {
-
+        try {
+            // Logic
+        } catch (Exception $e) {
+            $this->log->error("Upload variable interval failed for feed $feedid: " . $e->getMessage());
+        }
     }
 
-    /**
-     * Clear feed
-     *
-     * @param integer $feedid
-     * @return boolean true == success
-     */
     public function clear($feedid)
     {
-
+        try {
+            // Clear logic
+        } catch (Exception $e) {
+            $this->log->error("Clear feed failed for feed $feedid: " . $e->getMessage());
+            return false;
+        }
     }
 
-    /**
-     * clear out data from file before $start_time
-     *
-     * @param integer $feedid
-     * @param integer $start_time new timestamp to start the feed data from
-     * @return boolean
-     */
-    public function trim($feedid,$start_time)
+    public function trim($feedid, $start_time)
     {
-
+        try {
+            // Trim logic
+        } catch (Exception $e) {
+            $this->log->error("Trim feed failed for feed $feedid: " . $e->getMessage());
+            return false;
+        }
     }
+}
 // #### \/ Below engine public specific methods
 
 
 // #### \/ Bellow are engine private methods
 
-}
+
