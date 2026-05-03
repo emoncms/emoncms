5b50a43ddda824e5335888d7bb78aaa0f6c007fd
Trystan Lea <trystan.lea@gmail.com>
2026-05-03 22:14:39 +0100

    clean up

--- Modules/input/Views/modals/delete/delete_modal.js
+++ Modules/input/Views/modals/delete/delete_modal.js
@@ -1,37 +1,3 @@
-var input_dialog =
-{
-	inputid: null,
-	
-	'loadDelete': function(callback, inputid, tablerow){
-		this.inputid = inputid;
-		this.drawDelete(callback, tablerow);
-	},
-	
-	'drawDelete':function(callback, row){
-        emoncmsModal.open('inputDeleteDialog');
-		$('#inputDeleteModalLabel').html('Delete Input: <b>'+input_dialog.inputid+'</b>');
-		$("#inputDelete-confirm").off('click').on('click', function(){
-			$('#inputDelete-loader').show();
-			var result = input.remove(input_dialog.inputid);
-			$('#inputDelete-loader').hide();
-			/* TBD requires API changes
-			if (!result.success) {
-				alert('Unable to delete input:\n'+result.message);
-				return false;
-			} else {
-			*/
-				if (row != null) table.remove(row);
-				update();
-                    emoncmsModal.close('inputDeleteDialog');
-				if (typeof callback === "function") {
-					callback(true);
-				}
-			//}
-			return true;
-		});
-	}
-}
-
 var delete_input = Vue.createApp({
     data: function() {
         return {
