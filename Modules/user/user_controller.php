bb86150ca808535233b00a49347efe6c69a68e04
Trystan Lea <trystan.lea@gmail.com>
2026-05-07 19:38:32 +0100

    fix htmlspecialchars() Misapplied to URL in Logout Flow (Logic Bug)

--- Modules/user/user_controller.php
+++ Modules/user/user_controller.php
@@ -76,7 +76,20 @@ function user_controller()
             $msg = get('msg');
             $message = isset($msg) ? htmlspecialchars(urldecode($msg)) : '';
             $ref = get('ref');
-            $referrer = isset($ref) ? htmlspecialchars(urldecode(base64_decode(get('ref')))) : '';
+            
+            // Validate referrer using same logic as login section (prevent open redirect/XSS)
+            if(!is_null($ref)){
+                $decoded_ref = urldecode(base64_decode($ref));
+                $parsed = parse_url($decoded_ref);
+                // Only allow relative paths (no scheme, no host)
+                if (!isset($parsed['scheme']) && !isset($parsed['host']) && isset($parsed['path'])) {
+                    $referrer = $decoded_ref;  // Store as-is, don't htmlspecialchars here
+                } else {
+                    $referrer = '';
+                }
+            } else {
+                $referrer = '';
+            }
             
             // encode url parameters to pass through to login page
             $msg = urlencode($message);
