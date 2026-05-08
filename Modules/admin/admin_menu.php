ab5f38a0ea524a1ac85a05a2d24601cca873ffd7
Trystan Lea <trystan.lea@gmail.com>
2026-05-08 22:49:46 +0100

    remove everything from admin appart from admin userlist need to think how to modularise: info, components, updating, serial, firmware seperately not all installations need these in place

--- Modules/admin/admin_menu.php
+++ Modules/admin/admin_menu.php
@@ -4,47 +4,11 @@ if ($session["write"] && $session["admin"]) {
     $menu['setup']['l2']['admin'] = array(
         'name' => tr("Admin"),
         'href' => 'admin',
-        'default' => 'admin/info',
+        'default' => 'admin/users',
         'icon' => 'tasks',
         'order' => 13,
 
         "l3"=>array(
-            "info"=>array(
-                "name"=>tr("System Info"),
-                "href"=>"admin/info", 
-                "order"=>1, 
-                "icon"=>"input"
-            ),
-            "update"=>array(
-                "name"=>tr("Update"),
-                "href"=>"admin/update", 
-                "order"=>1, 
-                "icon"=>"input"
-            ),
-            "components"=>array(
-                "name"=>tr("Components"),
-                "href"=>"admin/components", 
-                "order"=>1, 
-                "icon"=>"input"
-            ),
-            "firmware"=>array(
-                "name"=>tr("Serial Monitor"),
-                "href"=>"admin/serial", 
-                "order"=>1, 
-                "icon"=>"input"
-            ),
-            "serialconfig"=>array(
-                "name"=>tr("Serial Config"),
-                "href"=>"admin/serconfig", 
-                "order"=>1, 
-                "icon"=>"input"
-            ),
-            "log"=>array(
-                "name"=>tr("Emoncms Log"),
-                "href"=>"admin/log", 
-                "order"=>1, 
-                "icon"=>"input"
-            ),
             "users"=>array(
                 "name"=>tr("Users"),
                 "href"=>"admin/users", 
