c9b23af0f5d052b42864c0778225103e18d7c289
Trystan Lea <trystan.lea@gmail.com>
2026-06-12 10:55:47 +0100

    move menu divider to input

--- Modules/input/input_menu.php
+++ Modules/input/input_menu.php
@@ -1,3 +1,17 @@
 <?php
 global $session;
-if ($session["write"]) $menu["setup"]["l2"]['input'] = array("name"=>tr("Inputs"),"href"=>"input/view", "order"=>1, "icon"=>"input");
+if ($session["write"]) {
+    $menu["setup"]["l2"]['input'] = array(
+        "name"=>tr("Inputs"),
+        "href"=>"input/view", 
+        "order"=>1, 
+        "icon"=>"input"
+    );
+    
+    $menu["setup"]["l2"]['divider'] = array(
+        "divider"=>"15px",
+        "href"=>'',
+        "order"=>6
+    );
+ 
+ }
